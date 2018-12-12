bd928dececd3c622d981879ea4a5185e49d65c0f
Maciej Goszczycki <mgoszcz2@gmail.com>
2018-12-12 18:22:33 +0000

    Add day 6

---
+++ day6.py
@@ -0,0 +1,29 @@
+import numpy as np
+from collections import Counter
+
+
+def closest(x):
+    distances = abs(coords - x).sum(1)
+    best = distances.argmin()
+    if sum(distances == distances[best]) > 1:
+        return -1
+    return best
+
+
+def within_10000(x):
+    return sum(abs(coords - x).sum(1)) < 10000
+
+
+def edge_set(a):
+    return set(a[:, -1]) | set(a[:, 0]) | set(a[0, :]) | set(a[-1, :]) | {-1}
+
+
+coords = np.array(
+    [tuple(map(int, x.split(", "))) for x in open("day6.txt").readlines()]
+)
+width = max(coords[:, 0]) + 1
+height = max(coords[:, 1]) + 1
+voronoi = np.apply_along_axis(closest, 0, np.indices((width, height)))
+infinite = edge_set(voronoi)
+print(Counter(x for x in voronoi.flat if x not in infinite).most_common(1)[0][1])
+print(np.apply_along_axis(within_10000, 0, np.indices((width, height))).sum())
