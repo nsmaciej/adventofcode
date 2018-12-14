7786d4509904c66b275b7f308b17c98ac278fbfe
Maciej Goszczycki <mgoszcz2@gmail.com>
2018-12-14 23:30:31 +0000

    Fast day 9 (and part b)

--- day9.py
+++ day9.py
@@ -1,24 +1,26 @@
 import re
-import array
+from collections import deque
 
-players, points = map(
+
+def solve(players, last_marble):
+    circle = deque([0])
+    score = [0] * players
+    for marble in range(1, last_marble + 1):
+        if marble % 23 == 0:
+            player = (marble - 1) % players
+            circle.rotate(7)
+            score[player] += circle.popleft() + marble
+        else:
+            circle.rotate(-2 if len(circle) > 2 else 0)
+            circle.appendleft(marble)
+    return max(score)
+
+
+players, last_marble = map(
     int,
     re.match(
         r"(\d+) players; last marble is worth (\d+) points", open("day9.txt").read()
     ).groups(),
 )
-
-score = [0] * players
-circle = array.array("i", [0])
-i = 0
-for marble in range(1, points + 1):
-    if marble % 23 == 0:
-        player = (marble - 1) % players
-        new_i = (i - 7) % len(circle)
-        score[player] += circle.pop(new_i) + marble
-        i = new_i
-    else:
-        i = (i + 2) % len(circle) if len(circle) > 2 else 1
-        circle.insert(i, marble)
-
-print(max(score))
+print(solve(players, last_marble))
+print(solve(players, last_marble * 100))
