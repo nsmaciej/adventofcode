f94f34abfad5c9d86069648da8d1d9d4732c3c42
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-03 18:18:13 +0000

    Improve day 3 2019 using dict.keys()

--- 2019/day03.py
+++ 2019/day03.py
@@ -1,6 +1,5 @@
 def points(turns):
     times = {}
-    point_set = set()
     x, y, t = 0, 0, 0
     for turn in turns.split(","):
         dx, dy = DIRECTIONS[turn[0]]
@@ -8,14 +7,14 @@ def points(turns):
             x += dx
             y += dy
             t += 1
-            point_set.add((x, y))
             times[(x, y)] = t
-    return point_set, times
+    return times
 
 
 a, b = open("inputs/day03.txt")
 DIRECTIONS = {"U": (0, 1), "D": (0, -1), "L": (-1, 0), "R": (1, 0)}
-a, at = points(a)
-b, bt = points(b)
-print(min(map(lambda x: abs(x[0]) + abs(x[1]), a & b)))
-print(min(map(lambda x: at[x] + bt[x], a & b)))
+a = points(a)
+b = points(b)
+intersections = set(a.keys()) & set(b.keys())
+print(min(map(lambda x: abs(x[0]) + abs(x[1]), intersections)))
+print(min(map(lambda x: a[x] + b[x], intersections)))
