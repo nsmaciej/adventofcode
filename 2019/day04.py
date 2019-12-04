eb0b7bddfce11c0aa2326c6d75c69da6314dac10
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-04 07:15:48 +0000

    Use numbers instead of itertools for day 4 2019

--- 2019/day04.py
+++ 2019/day04.py
@@ -1,30 +1,17 @@
-from itertools import product
 from collections import Counter
 
 
-def cmp(x, y):
-    for i, j in zip(x, y):
-        if i == j:
-            continue
-        return (i > j) - (i < j)
-    return 0
-
-
 def solve(group):
-    answers = 0
-    for attempt in product(range(10), repeat=len(start)):
-        if (
-            cmp(attempt, start) >= 0
-            and cmp(attempt, end) <= 0
-            and tuple(sorted(attempt)) == attempt
-            and any(group(x) for x in Counter(attempt).values())
-        ):
-            answers += 1
-    return answers
+    for x in range(start, end + 1):
+        digits = list(str(x))
+        yield (
+            x >= start
+            and x <= end
+            and sorted(digits) == digits
+            and any(group(i) for i in Counter(digits).values())
+        )
 
 
-start, end = map(
-    lambda x: list(map(int, x.strip())), open("inputs/day04.txt").read().split("-")
-)
-print(solve(lambda x: x >= 2))
-print(solve(lambda x: x == 2))
+start, end = map(int, open("inputs/day04.txt").read().split("-"))
+print(sum(solve(lambda x: x >= 2)))
+print(sum(solve(lambda x: x == 2)))
