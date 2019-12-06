850008b7fd171a1ff6537d7db529494e6b5afe12
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-06 05:30:57 +0000

    Clean up day 6

--- 2019/day06.py
+++ 2019/day06.py
@@ -1,20 +1,10 @@
-from collections import defaultdict
-
-
-def count(x):
-    return 1 + (count(orbits[x]) if x in orbits else 0)
-
-
 def trace(x):
     return (trace(orbits[x]) if x in orbits else []) + [x]
 
 
 data = [x.strip().split(")") for x in open("inputs/day06.txt")]
-orbits = {}
-for x, y in data:
-    orbits[y] = x
-
-print(sum(count(x) - 1 for x in orbits.keys()))
+orbits = {y: x for x, y in data}
 you, san = trace("YOU"), trace("SAN")
 offset = sum(x == y for x, y in zip(you, san))
-print(len(you) - you.index(you[offset]) + len(san) - san.index(san[offset]) - 2)
+print(sum(len(trace(x)) - 1 for x in orbits.keys()))
+print(len(you) - you.index(you[offset + 1]) + len(san) - san.index(san[offset + 1]))
