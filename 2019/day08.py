8b16e1d30575b0f04385154ab5f7956575298902
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-08 12:12:44 +0000

    Use numpy for day 8 2019

--- 2019/day08.py
+++ 2019/day08.py
@@ -1,22 +1,7 @@
-from collections import Counter
 import numpy as np
 
-data = open("inputs/day08.txt").read().strip()
-width, height = 25, 6
-best = 9999999999999
-for i in range(0, len(data), width * height):
-    c = Counter(data[i : i + width * height])
-    if c["0"] < best:
-        best = c["0"]
-        # print(c["0"], c["1"] * c["2"])
-
-answer = np.zeros((height, width))
-answer[:] = 2
-for i in range(0, len(data), width * height):
-    m = np.array(list(map(int, data[i : i + width * height]))).reshape((height, width))
-    # print(m)
-    answer = np.where(answer == 2, m, answer)
-
-print("P1")
-print(f"{width} {height}")
-print(" ".join(map(lambda x: str(int(0 if x == 1 else 1)), answer.flatten())))
+data = np.fromiter(open("inputs/day08.txt").read().strip(), int).reshape((-1, 6, 25))
+best = (data == 0).sum((1, 2)).argmin()
+print((data[best] == 1).sum() * (data[best] == 2).sum())
+image = np.apply_along_axis(lambda x: next(i for i in x if i != 2), 0, data)
+print("\n".join("".join(map(str, row)).replace("0", " ") for row in image))
