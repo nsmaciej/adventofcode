937d527304a31f2effbc6eefb51a6a4002f04b16
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-09 13:04:20 +0100

    Clean up parameter handling a lot, inline tape indexing, abstract over jumps

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -9,81 +9,68 @@ class Vm:
         self.relative_base = 0
         self.inputs = inputs or []
 
-    def paramater_index(self, arg, mode):
+    def arg_index(self, arg):
         ix = self.pc + 1 + arg
+        mode = self.tape[self.pc] // [100, 1000, 10000][arg] % 10
         if mode == 0:  # Indirect.
-            return self[ix]
+            return self.tape.get(ix, 0)
         elif mode == 1:  # Immediate.
             return ix
         elif mode == 2:  # Relative.
-            return self.relative_base + self[ix]
+            return self.relative_base + self.tape.get(ix, 0)
 
-    def __getitem__(self, ix):
-        assert ix >= 0
-        return self.tape.get(ix, 0)
+    def arg(self, arg):
+        return self.tape.get(self.arg_index(arg), 0)
 
-    def __setitem__(self, ix, value):
-        assert ix >= 0
-        self.tape[ix] = value
-
-    def paramater(self, arg, mode):
-        return self[self.paramater_index(arg, mode)]
-
-    def store(self, arg, mode, value):
-        self[self.paramater_index(arg, mode)] = value
-
-    def operand(self, modes, fn):
-        result = fn(self.paramater(0, modes[0]), self.paramater(1, modes[1]))
-        self.store(2, modes[2], result)
+    def function(self, fn):
+        self.tape[self.arg_index(2)] = fn(self.arg(0), self.arg(1))
         self.pc += 4
+
+    def jump(self, test):
+        if test(self.arg(0)):
+            self.pc = self.arg(1)
+        else:
+            self.pc += 3
 
     def complete(self):
         assert self.run()
         return self.outputs
 
-    # Returns True if halted.
     def run(self):
-        while self[self.pc] != 99:
-            inst = self[self.pc]
+        while self.tape[self.pc] != 99:
+            inst = self.tape[self.pc]
             op = inst % 100
-            modes = [(inst // 100) % 10, (inst // 1000) % 10, (inst // 10000) % 10]
 
             # Math operators.
             if op == 1:
-                self.operand(modes, operator.add)
+                self.function(operator.add)
             elif op == 2:
-                self.operand(modes, operator.mul)
+                self.function(operator.mul)
             elif op == 7:
-                self.operand(modes, operator.lt)
+                self.function(operator.lt)
             elif op == 8:
-                self.operand(modes, operator.eq)
+                self.function(operator.eq)
+
+            # Jumps.
+            elif op == 5:
+                self.jump(operator.truth)
+            elif op == 6:
+                self.jump(operator.not_)
 
             # I/O.
             elif op == 3:
                 if not self.inputs:
                     # Need more input.
                     return False
-                self.store(0, modes[0], self.inputs.pop(0))
+                self.tape[self.arg_index(0)] = self.inputs.pop(0)
                 self.pc += 2
             elif op == 4:
-                self.outputs.append(self.paramater(0, modes[0]))
+                self.outputs.append(self.arg(0))
                 self.pc += 2
-
-            # Jumps.
-            elif op == 5:
-                if self.paramater(0, modes[0]):
-                    self.pc = self.paramater(1, modes[1])
-                else:
-                    self.pc += 3
-            elif op == 6:
-                if not self.paramater(0, modes[0]):
-                    self.pc = self.paramater(1, modes[1])
-                else:
-                    self.pc += 3
 
             # Relative base
             elif op == 9:
-                self.relative_base += self.paramater(0, modes[0])
+                self.relative_base += self.arg(0)
                 self.pc += 2
 
             else:
