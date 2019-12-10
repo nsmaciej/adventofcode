933018dbcbb0f68b4f8a08fc4fb42ee2ba81a80a
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-10 22:34:39 +0100

    Clean up the part b a lot, other small nits

--- 2019/day10.py
+++ 2019/day10.py
@@ -1,34 +1,25 @@
 import numpy as np
 from math import gcd, atan2, pi
 from collections import defaultdict
-from functools import reduce
 
 
-def detects(coord):
-    coverage = defaultdict(list)
+def asteroid_angles(coord):
+    angles = defaultdict(list)
     for other, i in np.ndenumerate(field):
         if other != coord and i == "#":
             dy, dx = other[0] - coord[0], other[1] - coord[1]
             d = gcd(dy, dx)
-            coverage[dy // d, dx // d].append(other)
-    return coverage
+            angles[dy // d, dx // d].append(other)
+    return angles
 
 
 def angle(x):
     return (atan2(x[0], x[1]) + pi / 2) % (2 * pi)
 
 
-field = np.array([list(x.strip()) for x in open("inputs/day10.txt")])
-coverage = {i: detects(i) for i, v in np.ndenumerate(field) if v == "#"}
-station = max(coverage, key=lambda x: len(coverage[x]))
-targets = coverage[station]
+field = [list(x.strip()) for x in open("inputs/day10.txt")]
+asteroids = {i: asteroid_angles(i) for i, v in np.ndenumerate(field) if v == "#"}
+targets = max(asteroids.values(), key=len)
 print(len(targets))
-
-angles = sorted(targets.keys(), key=angle)
-current_angle = 0
-for i in range(200):
-    while not targets[angles[current_angle]]:
-        current_angle = (current_angle + 1) % len(targets)
-    target = targets[angles[current_angle]].pop()
-    current_angle += 1
-print(target[1] * 100 + target[0])
+win_y, win_x = targets[sorted(targets.keys(), key=angle)[199]][0]
+print(win_x * 100 + win_y)
