02d0a1ed078cbb142f6ada3681f2eab147fb0a84
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-13 13:59:46 +0000

    Clean up day 13, OOify intcode more

--- 2019/day13.py
+++ 2019/day13.py
@@ -1,43 +1,24 @@
 from intcode import Vm
-import time
-
 
 program = open("inputs/day13.txt").read()
+
 print(Vm(program).complete()[2::3].count(2))
-
-
-def render():
-    print("\033[2J\033[H", end="")
-    print(score)
-    tiles = {0: " ", 1: "#", 2: ".", 3: "^", 4: "@"}
-    xs = [i[0] for i in screen]
-    ys = [i[1] for i in screen]
-    for x in range(min(ys), max(ys) + 1):
-        print(
-            "".join(tiles[screen.get((y, x), 0)] for y in range(min(xs), max(xs) + 1))
-        )
-    print()
-
 
 game = Vm(program)
 game.tape[0] = 2
 screen = {}
 score = 0
-last_pos = {}
+last_x = {}
 game.run()
-while True:
+while game.has_output():
     output = game.drain_output()
     for i in range(0, len(output), 3):
         x, y, k = output[i : i + 3]
         if x == -1 and y == 0:
             score = k
         else:
-            last_pos[k] = (x, y)
+            last_x[k] = x
             screen[x, y] = k
-    ball_y = last_pos[4][0]
-    paddle_y = last_pos[3][0]
-    game.input((paddle_y < ball_y) - (paddle_y > ball_y))
-    if 2 not in screen.values():
-        break
+    game.input((last_x[3] < last_x[4]) - (last_x[3] > last_x[4]))
     game.run()
 print(score)
--- 2019/intcode.py
+++ 2019/intcode.py
@@ -5,85 +5,88 @@ import sys
 class Vm:
     def __init__(self, program, inputs=None):
         self.tape = dict(enumerate(map(int, program.split(","))))
-        self.pc = 0
-        self.outputs = []
-        self.relative_base = 0
-        self.inputs = inputs or []
+        self._pc = 0
+        self._outputs = []
+        self._base = 0
+        self._inputs = inputs or []
 
-    def arg_index(self, arg):
-        ix = self.pc + 1 + arg
-        mode = self.tape[self.pc] // [100, 1000, 10000][arg] % 10
+    def _arg_index(self, arg):
+        ix = self._pc + 1 + arg
+        mode = self.tape[self._pc] // [100, 1000, 10000][arg] % 10
         if mode == 0:  # Indirect.
             return self.tape.get(ix, 0)
         elif mode == 1:  # Immediate.
             return ix
         elif mode == 2:  # Relative.
-            return self.relative_base + self.tape.get(ix, 0)
+            return self._base + self.tape.get(ix, 0)
 
-    def arg(self, arg):
-        return self.tape.get(self.arg_index(arg), 0)
+    def _arg(self, arg):
+        return self.tape.get(self._arg_index(arg), 0)
 
-    def function(self, fn):
-        self.tape[self.arg_index(2)] = fn(self.arg(0), self.arg(1))
-        self.pc += 4
+    def _function(self, fn):
+        self.tape[self._arg_index(2)] = fn(self._arg(0), self._arg(1))
+        self._pc += 4
 
-    def jump(self, test):
-        if test(self.arg(0)):
-            self.pc = self.arg(1)
+    def _jump(self, test):
+        if test(self._arg(0)):
+            self._pc = self._arg(1)
         else:
-            self.pc += 3
+            self._pc += 3
+
+    def has_output(self):
+        return bool(self._outputs)
 
     def output(self):
-        return self.outputs.pop(0)
+        return self._outputs.pop(0)
 
     def drain_output(self):
-        output = self.outputs
-        self.outputs = []
+        output = self._outputs
+        self._outputs = []
         return output
 
     def input(self, value):
-        self.inputs.append(value)
+        self._inputs.append(value)
 
     def complete(self):
         assert self.run()
-        return self.outputs
+        return self._outputs
 
     def run(self):
-        while self.tape[self.pc] != 99:
-            inst = self.tape[self.pc]
+        while self.tape[self._pc] != 99:
+            inst = self.tape[self._pc]
             op = inst % 100
 
             # Math operators.
             if op == 1:
-                self.function(operator.add)
+                self._function(operator.add)
             elif op == 2:
-                self.function(operator.mul)
+                self._function(operator.mul)
             elif op == 7:
-                self.function(operator.lt)
+                self._function(operator.lt)
             elif op == 8:
-                self.function(operator.eq)
+                self._function(operator.eq)
 
-            # Jumps.
+            # _jumps.
             elif op == 5:
-                self.jump(operator.truth)
+                self._jump(operator.truth)
             elif op == 6:
-                self.jump(operator.not_)
+                self._jump(operator.not_)
 
             # I/O.
             elif op == 3:
-                if not self.inputs:
+                if not self._inputs:
                     # Need more input.
                     return False
-                self.tape[self.arg_index(0)] = self.inputs.pop(0)
-                self.pc += 2
+                self.tape[self._arg_index(0)] = self._inputs.pop(0)
+                self._pc += 2
             elif op == 4:
-                self.outputs.append(self.arg(0))
-                self.pc += 2
+                self._outputs.append(self._arg(0))
+                self._pc += 2
 
             # Relative base
             elif op == 9:
-                self.relative_base += self.arg(0)
-                self.pc += 2
+                self._base += self._arg(0)
+                self._pc += 2
 
             else:
                 assert op == 99
