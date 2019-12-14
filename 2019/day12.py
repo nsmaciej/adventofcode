0f7b577db884e7823077bc1719a7c4cc3ee3c74b
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-14 07:47:50 +0000

    Actually make Eoin's optimisation for day 12 work

--- 2019/day12.py
+++ 2019/day12.py
@@ -10,12 +10,14 @@ class Universe:
     def __init__(self, moons):
         self.moons = np.copy(moons)
         self.vel = np.zeros_like(moons)
+        self.age = 0
 
     def step(self):
         for i in range(len(self.moons)):
             x, y = self.moons, self.moons[i, :]
             self.vel[i] += ((x > y).astype(int) - (x < y).astype(int)).sum(0)
         self.moons += self.vel
+        self.age += 1
 
     def energy(self):
         return sum(abs(self.moons).sum(1) * abs(self.vel).sum(1))
@@ -29,11 +31,9 @@ print(universe.energy())
 
 universe = Universe(moons)
 cycle = {}
-age = 0
 while len(cycle) < 3:
     universe.step()
     for i in range(3):
-        if not universe.vel[i, :].any():
-            cycle[i] = age
-    age += 1
+        if not universe.vel[:, i].any() and i not in cycle:
+            cycle[i] = 2 * universe.age
 print(np.lcm.reduce(list(cycle.values())))
