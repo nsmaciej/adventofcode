4d4e8c60a6249f648e4e308dfe010f6bab17e4ac
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-14 06:32:43 +0000

    Do part 2 day 14

--- 2019/day14.py
+++ 2019/day14.py
@@ -1,29 +1,8 @@
-from collections import Counter, namedtuple, defaultdict
+from collections import namedtuple, defaultdict
 import math
 
-Reaction = namedtuple("Reaction", ["product_count", "reactants"])
-
-made = 0
-
-
-def make(kind, q):
-    global made
-    if kind == "ORE":
-        made += q
-    elif stock[kind] >= q:
-        stock[kind] -= q
-    else:
-        q -= stock[kind]
-        stock[kind] = 0
-        runs = math.ceil(q / reactions[kind].product_count)
-        for _ in range(runs):
-            for i, iq in reactions[kind].reactants:
-                make(i, iq)
-        stock[kind] = reactions[kind].product_count * runs - q
-
-
 reactions = {}
-stock = defaultdict(int)
+Reaction = namedtuple("Reaction", ["product_count", "reactants"])
 for line in open("inputs/day14.txt"):
     reactants, product = line.split(" => ")
     product_count, product_type = product.split()
@@ -31,5 +10,33 @@ for line in open("inputs/day14.txt"):
     reactants = [(y, int(x)) for x, y in reactants]
     reactions[product_type] = Reaction(int(product_count), reactants)
 
-make("FUEL", 1)
-print(made)
+
+def make(fuel_amount):
+    needs = defaultdict(int)
+    needs["FUEL"] = fuel_amount
+    ore_used = 0
+    while any(x > 0 for x in needs.values()):
+        target = next(x for x in needs if needs[x] > 0)
+        target_count = needs[target]
+        per_reaction = reactions[target].product_count
+        to_produce = math.ceil(target_count / per_reaction)
+        needs[target] -= to_produce * per_reaction
+        for i, iq in reactions[target].reactants:
+            if i == "ORE":
+                ore_used += iq * to_produce
+            else:
+                needs[i] += iq * to_produce
+    return ore_used
+
+
+print(make(1))
+target = 1000000000000
+low, high = 0, target
+while low < high:
+    mid = (low + high) // 2
+    req = make(mid)
+    if req > target:
+        high = mid - 1
+    elif req < target:
+        low = mid
+print(low)
