f9c1c92574ca4e5b4926c25fe0d81e1207942de0
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-17 17:32:44 +0000

    Not WIP version of the python version of day 16 2019

--- 2019/day16.py
+++ 2019/day16.py
@@ -1,20 +1,14 @@
 from aoc import *
 
-phase = list(map(int, data(16).read().strip()))
-# phase = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
-def run(phase, offset):
-    phase = phase[offset : offset + 8]
+def run(phase):
     for p in range(100):
-        print(p)
-        print("".join(map(str, phase)))
-        phase = [apply(phase, offset, i) for i in range(len(phase))]
-    print("".join(map(str, phase)))
-    return "".join(map(str, phase))
+        phase = [digit(phase, i) for i in range(len(phase))]
+    return "".join(map(str, phase[:8]))
 
 
-def apply(phase, offset, ix):
+def digit(phase, ix):
     pat = [0, 1, 0, -1]
     pi, pl = 0, ix
     r = 0
@@ -29,4 +23,5 @@ def apply(phase, offset, ix):
     return abs(r) % 10
 
 
-print(run(phase, 0))
+phase = list(map(int, data(16).read().strip()))
+print(run(phase))
