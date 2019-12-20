a2ef1b1efb7bb121689b0e32167dad7e4dad56a4
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-20 01:34:12 +0000

    Much faster day 18 assuming it's acylic (but still using BFS to deal with the middle)

--- 2019/day18.py
+++ 2019/day18.py
@@ -4,15 +4,20 @@ from collections import deque
 
 def distance(start):
     queue = deque([start])
-    dist = {}
+    dist, needed = {}, {}
     dist[start] = 0
+    needed[start] = set()
     while queue:
         pos = queue.popleft()
         for p in around(pos):
-            if p in grid and grid[p] != "#" and not grid[p].isupper() and p not in dist:
+            if p in grid and grid[p] != "#" and p not in dist:
+                val = grid[p]
                 dist[p] = dist[pos] + 1
+                needed[p] = needed[pos] | ({val.lower()} if val.isupper() else set())
                 queue.append(p)
-    return {k: dist[key_pos[k]] for k in keys if key_pos[k] in dist}
+    return {
+        k: (dist[key_pos[k]], needed[key_pos[k]]) for k in keys if key_pos[k] in dist
+    }
 
 
 def solve():
@@ -26,12 +31,11 @@ def solve():
                 shortest = td
             return
         dp[dp_key] = td
-        key_dist = distance(pos)
         for key in keys - got:
-            if key in key_dist:
+            if dist[pos][key][1] <= got:
                 if key in door_pos:
                     grid[door_pos[key]] = "."
-                inner(key_pos[key], got | {key}, td + key_dist[key])
+                inner(key_pos[key], got | {key}, td + dist[pos][key][0])
                 if key in door_pos:
                     grid[door_pos[key]] = key.upper()
 
@@ -48,5 +52,9 @@ grid[you] = "."
 # Compute some useful data.
 keys = {v for k, v in grid.items() if v.islower()}
 key_pos = {v: k for k, v in grid.items() if v.islower()}
+key_ix = {k: i for i, k in enumerate(keys)}
 door_pos = {v.lower(): k for k, v in grid.items() if v.isupper()}
+dist = {you: distance(you)}
+for k in keys:
+    dist[key_pos[k]] = distance(key_pos[k])
 print(solve())
