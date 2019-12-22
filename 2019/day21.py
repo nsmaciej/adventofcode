9f137e5d5306e0b0f6d9a13c486c31494e6b0e75
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-22 20:56:16 +0000

    Much shorter part 2

--- 2019/day21.py
+++ 2019/day21.py
@@ -11,13 +11,12 @@ def run(program):
     for line in program.strip().splitlines():
         if not line.startswith("#") and line:
             droid.input_line(line)
-    return droid.complete()
+    print(droid.complete()[-1])
 
 
-print(
-    run(
-        """
-# Jump if: C air, D land
+# D ^ ~C ^ ~A
+run(
+    """
 NOT C J
 AND D J
 # Jump if: A air
@@ -25,48 +24,19 @@ NOT A T
 OR T J
 WALK
 """
-    )[-1]
 )
 
-# @#..#.###
-# @##.##.##
-# @##.##..#
-# @##.#..##
-# @#.##.###
-# @##.###.#
-# @##.#.##. (not)
-# @####.### (not)
-# @####..## (not)
 
-# Jump if we have somewhere to land, again so one jump later and there is some air to
-# the left and right of the first land block. Otherwise jump if on edge.
-# D=land and H=land and (B=air or C=air) and (E=air or F=air or G=air) or a=air
-# D ^ H ^ (~B v ~C) ^ (~E v ~F v ~G) | ~A
-# D ^ H ^ ~(B ^ C) ^ ~(E ^ F ^ G) | ~A
-print(
-    run(
-        """
-# ~(B ^ C)
+# D ^ H ^ ~(B ^ C) ^ ~A
+run(
+    """
 OR B T
 AND C T
 NOT T J
-
-# ^ ~(E ^ F ^ G)
-OR E T
-AND F T
-AND G T
-NOT T T
-AND T J
-
-# ^ D
 AND D J
-# ^ H
 AND H J
-
 NOT A T
 OR T J
 RUN
 """
-    )[-1]
 )
-
