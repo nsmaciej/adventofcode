0c788596c8cbf058e24592842a460cfbdbbffab4
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-22 15:07:29 +0000

    Clean up explanation

--- 2019/day21.py
+++ 2019/day21.py
@@ -28,36 +28,39 @@ WALK
     )[-1]
 )
 
-print(
-    run(
-        """
-# @#..#.###        @#..L.##L
-# @##.##.##        @##.L#_#L
-# @##.##..#        @##.L#..L
-# @##.#..##        @##.L..#L
-# @#.##.###        @#.#L..#L (note: C and F not air)
-# @##.###.#        @##.###.#
-# @##.#.##.        @##.L.##.       (not)
-# @####.###(.##.)  @###L.##L(.##.) (not)
-# @####..##(.##.)  @###L..#L(.##.) (not)
+# @#..#.###
+# @##.##.##
+# @##.##..#
+# @##.#..##
+# @#.##.###
+# @##.###.#
+# @##.#.##. (not)
+# @####.### (not)
+# @####..## (not)
 
 # Jump if we have somewhere to land, again so one jump later and there is some air to
 # the left and right of the first land block. Otherwise jump if on edge.
-# D=land and H=land and (B=air or C=air) and (E=air or F=air or G=air)
-# D ^ H ^ (~B v ~C) ^ (~E v ~F v ~G)
-# D ^ H ^ ~(B ^ C) ^ ~(E ^ F ^ G)
-
+# D=land and H=land and (B=air or C=air) and (E=air or F=air or G=air) or a=air
+# D ^ H ^ (~B v ~C) ^ (~E v ~F v ~G) | ~A
+# D ^ H ^ ~(B ^ C) ^ ~(E ^ F ^ G) | ~A
+print(
+    run(
+        """
+# ~(B ^ C)
 OR B T
 AND C T
 NOT T J
 
+# ^ ~(E ^ F ^ G)
 OR E T
 AND F T
 AND G T
 NOT T T
 AND T J
 
+# ^ D
 AND D J
+# ^ H
 AND H J
 
 NOT A T
