82b5b527628602d6ea821749d7c03fb6fbcdfff8
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-24 17:23:35 +0100

    Don't actually make a list for day 22 part 1 (in preparation for part 2)

--- 2019/day22.py
+++ 2019/day22.py
@@ -2,22 +2,20 @@ from aoc import *
 import re
 
 cmds = list(data(22).read().splitlines())
-cards = list(range(10007))
+N = 10007
 
-for cmd in cmds:
-    if cmd == "deal into new stack":
-        cards.reverse()
+def at_index(i):
+    for cmd in reversed(cmds):
+        if cmd == "deal into new stack":
+            i = N - 1 - i
 
-    elif m := re.match("cut (-?[0-9]+)", cmd):
-        val = int(m[1])
-        cards = cards[val:] + cards[:val]
+        elif m := re.match("cut (-?[0-9]+)", cmd):
+            val = int(m[1])
+            i = (val + i) % N
 
-    elif m := re.match("deal with increment ([0-9]+)", cmd):
-        val = int(m[1])
-        j = 0
-        new_cards = cards.copy()
-        for i in range(len(cards)):
-            new_cards[j] = cards[i]
-            j = (j + val) % len(cards)
-        cards = new_cards
-print(cards.index(2019))
+        elif m := re.match("deal with increment ([0-9]+)", cmd):
+            val = int(m[1])
+            i = (i * pow(val, -1, N)) % N
+    return i
+
+print(next(x for x in range(N) if at_index(x) == 2019))
