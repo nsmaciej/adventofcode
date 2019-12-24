18393497bc4946006023e29f44e2102a45f7fa28
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-24 07:28:31 +0000

    Day 23 part 2

--- 2019/day23.py
+++ 2019/day23.py
@@ -5,6 +5,8 @@ from intcode import Vm, Program
 def simulate():
     computers = [Vm(program, [i]) for i in range(50)]
     while True:
+        idle = True
+        nat = None
         for c in computers:
             if not c.has_input():
                 c.input(-1)
@@ -15,10 +17,23 @@ def simulate():
                 for i in range(0, len(otuput), 3):
                     dest, x, y = otuput[i : i + 3]
                     if dest == 255:
-                        return y
-                    computers[dest].input(x)
-                    computers[dest].input(y)
+                        nat = x, y
+                    else:
+                        computers[dest].input(x)
+                        computers[dest].input(y)
+                        idle = False
+        if idle and nat and not any(c.has_input() for c in computers):
+            x, y = nat
+            yield y
+            computers[0].input(x)
+            computers[0].input(y)
 
 
 program = Program(data(23).read())
-print(simulate())
+print(next(simulate()))
+seen = set()
+for y in simulate():
+    if y in seen:
+        print(y)
+        break
+    seen.add(y)
