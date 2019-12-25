4523063f09791e1f415416addfeedd4fac0de089
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-25 10:53:51 +0100

    Express day 22 part 1 as a linear function

--- 2019/day22.py
+++ 2019/day22.py
@@ -5,19 +5,24 @@ cmds = list(data(22).read().splitlines())
 N = 10007
 
 
-def at_index(i):
+def linear_fn():
+    a, b = 1, 0
     for cmd in reversed(cmds):
         if cmd == "deal into new stack":
-            i = N - 1 - i
+            a *= -1
+            b = N - 1 - b
 
         elif m := re.match("cut (-?[0-9]+)", cmd):
-            val = int(m[1])
-            i = (val + i) % N
+            b += int(m[1])
 
         elif m := re.match("deal with increment ([0-9]+)", cmd):
-            val = int(m[1])
-            i = (i * pow(val, -1, N)) % N
-    return i
+            m = pow(int(m[1]), -1, N)
+            a *= m
+            b *= m
+        a %= N
+        b %= N
+    return a, b
 
 
-print(next(x for x in range(N) if at_index(x) == 2019))
+a, b = linear_fn()
+print(next(i for i in range(N) if (a * i + b) % N == 2019))
