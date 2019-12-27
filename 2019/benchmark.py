a12ab003921b4710a0475e57587517b3ad039511
Maciej Goszczycki <mgoszcz2@gmail.com>
2019-12-27 12:59:58 +0100

    Benchmark everything

--- 2019/benchmark.py
+++ 2019/benchmark.py
@@ -5,32 +5,36 @@ import os
 import sys
 
 
-def time_one(day, fast):
-    start = perf_counter()
-    subprocess.check_call(
-        ["python3", f"day{day:02}.py"],
-        stdout=subprocess.DEVNULL,
-        env={**os.environ, "PY_INTCODE": str(fast)},
-    )
-    return perf_counter() - start
+def time(day, slow):
+    times = []
+    for i in range(runs):
+        start = perf_counter()
+        subprocess.check_call(
+            ["python3", f"day{day:02}.py"],
+            stdout=subprocess.DEVNULL,
+            env={**os.environ, "PY_INTCODE": str(slow)},
+        )
+        times.append(perf_counter() - start)
+    return statistics.mean(times)
 
 
-def time(t):
-    return f"\033[1;31m{t:6.2f}s\033[0m" if t > 0.3 else f"{t:6.2f}s"
+def show(t, threshold=0.3):
+    return f"\033[1;31m{t:5.2f}s\033[0m" if t > threshold else f"{t:5.2f}s"
 
 
-def get_speedup(day):
-    before = statistics.mean(time_one(day, 1) for _ in range(runs))
-    after = statistics.mean(time_one(day, 0) for _ in range(runs))
-    print(f"Day {day:<2}  {before / after:4.1f}x ({time(before)}, {time(after)})")
-    return before, after
+def test(day):
+    result = time(day, 0)
+    if day in intcode_days:
+        before = time(day, 1)
+        print(f"Day {day:>2} {show(result)}   ({before / result:4.1f}x {show(before)})")
+    else:
+        print(f"Day {day:>2} {show(result)}")
+    return result
 
 
-runs = 10
-days = [2, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25]
-days = list(map(int, sys.argv[1:])) if len(sys.argv) > 1 else days
+runs = 2
+intcode_days = [2, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25]
+test_days = map(int, sys.argv[1:]) if len(sys.argv) > 1 else range(1, 26)
 print(f"Using {runs} runs\n")
-stats = list(map(get_speedup, days))
-before = sum(x for x, _ in stats)
-after = sum(y for _, y in stats)
-print(f"\nOverall\t{before / after:4.1f}x ({before:6.2f}s, {after:6.2f}s)")
+total = sum(map(test, test_days))
+print(f"\nTotal  {show(total, threshold=10)}")
