dfaa8cdc4d6855e9d7f99449b343f5cacc027aeb
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-02 02:55:11 +0100

    Day 3 (2015)

---
+++ 2015/Day03.hs
@@ -0,0 +1,26 @@
+import Data.Set (Set)
+import qualified Data.Set as S
+import Soln
+
+main = runSoln' init part1 part2
+
+part1 :: String -> Int
+part1 = S.size . fly
+
+part2 :: String -> Int
+part2 xs = S.size $ fly l `S.union` fly r where (l, r) = uninterleave xs
+
+uninterleave :: [a] -> ([a], [a])
+uninterleave (x : y : r) = (x : xs, y : ys) where (xs, ys) = uninterleave r
+uninterleave x = (x, [])
+
+fly :: String -> Set (Pt Int)
+fly = snd . foldl (\(p, v) m -> (p +> m, S.insert (p +> m) v)) (zero, S.singleton zero)
+
+(+>) :: (Pt Int) -> Char -> (Pt Int)
+p +> c = p + fromPair (dir c)
+  where
+    dir '<' = (0, -1)
+    dir '>' = (0, 1)
+    dir '^' = (-1, 0)
+    dir 'v' = (1, 0)
--- 2015/Soln.hs
+++ 2015/Soln.hs
@@ -1,14 +1,34 @@
-module Soln (runSoln, runSoln') where
+module Soln (Pt, fromPair, zero, runSoln, runSoln') where
 
 import Data.Char
-import System.Environment
+import System.Environment (getArgs, getProgName)
 
 runSoln :: (String -> IO ()) -> (String -> IO ()) -> IO ()
 runSoln f g = do
+  args <- getArgs
   day <- filter isDigit <$> getProgName
-  input <- readFile $ "inputs/day" ++ day ++ ".txt"
+  let file = case args of
+        ["-"] -> "/dev/stdin"
+        [m] -> m
+        _ -> "inputs/day" ++ day ++ ".txt"
+  input <-  readFile file
   f input
   g input
 
 runSoln' :: (Show l, Show r) => (String -> a) -> (a -> l) -> (a -> r) -> IO ()
 runSoln' p f g = runSoln (print . f . p) (print . g . p)
+data Pt a = Pt a a deriving (Eq, Ord, Show)
+
+instance Num a => Num (Pt a) where
+  (Pt x y) + (Pt x' y') = Pt (x + x') (y + y')
+  (Pt x y) * (Pt x' y') = Pt (x * x') (y * y')
+  negate (Pt x y) = Pt (- x) (- y)
+  abs (Pt x y) = Pt (abs x) (abs y)
+  signum (Pt x y) = Pt (signum x) (signum y)
+  fromInteger n = Pt (fromInteger n) (fromInteger n)
+
+fromPair :: Num a => (a, a) -> Pt a
+fromPair (x, y) = Pt x y
+
+zero :: Num a => Pt a
+zero = Pt 0 0
