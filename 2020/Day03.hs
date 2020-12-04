a89550be83f8bdda86b48a6165228f6c6f08df26
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-04 02:32:55 +0100

    Day 3 (2020)

---
+++ 2020/Day03.hs
@@ -0,0 +1,11 @@
+import Advent
+
+main = runSoln' lines (solve (3, 1)) (\x -> product $ map (`solve` x) ds)
+  where
+    ds = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+solve :: (Int, Int) -> [String] -> Int
+solve (dx, dy) = f 0
+  where
+    f x (t : ts) = (if t !! (x `mod` length t) == '#' then 1 else 0) + f (x + dx) (drop (dy - 1) ts)
+    f _ [] = 0
