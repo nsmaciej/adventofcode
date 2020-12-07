555a9098c2888b44d35af0cdab8de0226448af92
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-07 22:45:05 +0100

    Day 7 (2020)

---
+++ 2020/Day07.hs
@@ -0,0 +1,29 @@
+import Advent
+import Data.Map (Map)
+import qualified Data.Map as M
+
+type Bag = (Int, String)
+
+main = runSoln'
+  (M.fromList . parseLines pRule)
+  (\bs -> countp (contains bs "shiny gold") $ M.keys bs)
+  (depCount "shiny gold")
+
+contains :: Map String [Bag] -> String -> String -> Bool
+contains bs t x = elem t cs || any (contains bs t) cs
+  where
+    cs = map snd $ bs M.! x
+
+depCount :: String -> Map String [Bag] -> Int
+depCount t bs = sum . map (\(i, x) -> i + i * depCount x bs) $ bs M.! t
+
+pRule :: Parser (String, [Bag])
+pRule = (,)
+  <$> pName <* string " contain "
+  <*> (pure [] <* string "no other bags." <|> pBag `sepBy1` string ", ")
+
+pBag :: Parser Bag
+pBag = (,) <$> decimal <* char ' ' <*> pName
+
+pName :: Parser String
+pName = manyTill (lowerChar <|> char ' ') (string " bags" <|> string " bag")
