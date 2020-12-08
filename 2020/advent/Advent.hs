2fde31e5de11121067343e5598b26069aec59639
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-08 08:05:20 +0100

    Print runtime in Advent (2020)

--- 2020/advent/Advent.hs
+++ 2020/advent/Advent.hs
@@ -10,7 +10,9 @@ module Advent (
 import Data.Char
 import Data.List
 import System.Environment
+import System.CPUTime
 import Data.Void
+import Text.Printf
 import Text.Megaparsec hiding (parse)
 import Text.Megaparsec.Char (char, letterChar, lowerChar, spaceChar, string, newline, hspace, hspace1)
 import Text.Megaparsec.Char.Lexer (decimal)
@@ -23,8 +25,11 @@ inputFilePath = do
 runSoln :: (String -> IO ()) -> (String -> IO ()) -> IO ()
 runSoln f g = do
   input <- readFile =<< inputFilePath
+  start <- getCPUTime
   f input
   g input
+  end <- getCPUTime
+  printf "Took %.3fs\n" (fromInteger (end - start) * 1e-12 :: Double)
 
 countp :: (a -> Bool) -> [a] -> Int
 countp = (length .) . filter
