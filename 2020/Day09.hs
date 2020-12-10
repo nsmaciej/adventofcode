e9e8b0442734221807a1641d0b271faf41abea37
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-10 08:10:12 +0100

    Day 9 (2020)

---
+++ 2020/Day09.hs
@@ -0,0 +1,17 @@
+import Advent
+import Data.Maybe
+
+main = runSoln' (map read . lines) part1 part2
+
+part1 :: [Int] -> Int
+part1 xs = k
+  where
+    ws = map (\x -> (x !! 25, take 25 x)) $ tails xs
+    Just (k, _) = find (not . uncurry sums) ws
+
+sums :: Int -> [Int] -> Bool
+sums n ds = or [x + y == n | (x:xs) <- tails ds, y <- xs]
+
+part2 :: [Int] -> Int
+part2 ds = maximum rs + minimum rs
+  where rs = head [ys | xs <- inits ds, ys <- tails xs, sum ys == part1 ds]
