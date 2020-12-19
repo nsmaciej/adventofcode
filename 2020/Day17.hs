e957cd00b775a87998c99267e1f5a6360f1de069
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-19 04:16:14 +0100

    3x performance on day 17 (2020)
    Use a strict point and tuples

--- 2020/Day17.hs
+++ 2020/Day17.hs
@@ -1,27 +1,31 @@
 {-# LANGUAGE BangPatterns #-}
 import Advent (runSoln')
-import Control.Monad (liftM2)
 import Data.Set (Set)
 import qualified Data.Set as S
 
-type Point = [Int]
+data Point = P !Int !Int !Int !Int deriving (Show, Eq, Ord)
 type World = Set Point
 type Around = Point -> [Point]
 
-main = runSoln' world (solve 3) (solve 4 . S.map (0 :))
+main = runSoln' world (solve False) (solve True)
 
-solve :: Int -> World -> Int
-solve d w = S.size $ iterate (step d) w !! 6
+solve :: Bool -> World -> Int
+solve p2 w = S.size $ iterate (step p2) w !! 6
 
-step :: Int -> World -> World
-step d w = S.union kept new
+step :: Bool -> World -> World
+step p2 w = S.union kept new
   where
     kept = S.filter ((`elem` [3,4]) . neighbors) w
-    new = S.filter ((== 3) . neighbors) . S.fromList $ concatMap (around d) w
-    neighbors = length . filter (`S.member` w) . (around d)
+    new = S.filter ((== 3) . neighbors) . S.fromList $ concatMap (around p2) w
+    neighbors = length . filter (`S.member` w) . (around p2)
 
-around :: Int -> Point -> [Point]
-around d p = map (zipWith (+) p) $ iterate (liftM2 (:) [-1..1]) [[]] !! d
+around :: Bool -> Point -> [Point]
+around vary_w (P w' x' y' z') = [P (w' + w) (x' + x) (y' + y) (z' + z) |
+  w <- if vary_w then [0] else [-1..1],
+  x <- [-1..1],
+  y <- [-1..1],
+  z <- [-1..1]
+  ]
 
 world :: String -> World
-world str = S.fromList [[0, y, x] | (y, xs) <- zip [0..] (lines str), (x, c) <- zip [0..] xs, c == '#']
+world str = S.fromList [P 0 0 y x | (y, xs) <- zip [0..] (lines str), (x, c) <- zip [0..] xs, c == '#']
