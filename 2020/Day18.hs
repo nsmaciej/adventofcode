d01485daf98b02d2d975c432362a187ed87101fa
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-19 22:15:00 +0100

    Day 18 (2020)

---
+++ 2020/Day18.hs
@@ -0,0 +1,23 @@
+import Advent
+import Control.Applicative ((<**>))
+
+main = runSoln' id (sum . parseLines pExpr) (sum . parseLines pExpr')
+
+sym :: String -> Parser String
+sym str = string str <* hspace
+
+chainl :: Parser a -> Parser (a -> a -> a) -> Parser a
+chainl term op = term <**> rest
+  where
+    rest = (\op rhs rest lhs -> rest (op lhs rhs)) <$> op <*> term <*> rest <|> pure id
+
+pTerm :: Parser Int -> Parser Int
+pTerm expr = decimal <* hspace <|> between (sym "(") (sym ")") expr
+
+pExpr :: Parser Int
+pExpr = chainl (pTerm pExpr) ((+) <$ sym "+" <|> (*) <$ sym "*")
+
+pExpr' :: Parser Int
+pExpr' = chainl term ((*) <$ sym "*") <|> term
+  where
+    term = chainl (pTerm pExpr') ((+) <$ sym "+")
