03bea30651524e9b1e84030f32e6320e5e03b80d
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-19 04:28:30 +0100

    Get rid of some boolean blindness, day 17 (2020)

--- 2020/Day17.hs
+++ 2020/Day17.hs
@@ -1,29 +1,28 @@
-{-# LANGUAGE BangPatterns #-}
 import Advent (runSoln')
 import Data.Set (Set)
 import qualified Data.Set as S
 import Control.Monad (replicateM)
 
 data Point = P !Int !Int !Int !Int deriving (Show, Eq, Ord)
+data VaryW = ZeroW | VaryW deriving (Show, Eq)
 type World = Set Point
-type Around = Point -> [Point]
 
-main = runSoln' world (solve False) (solve True)
+main = runSoln' world (solve ZeroW) (solve VaryW)
 
-solve :: Bool -> World -> Int
+solve :: VaryW -> World -> Int
 solve p2 w = S.size $ iterate (step p2) w !! 6
 
-step :: Bool -> World -> World
+step :: VaryW -> World -> World
 step p2 w = S.union kept new
   where
-    kept = S.filter ((`elem` [3,4]) . neighbors) w
-    new = S.filter ((== 3) . neighbors) . S.fromList $ concatMap (around p2) w
-    neighbors = length . filter (`S.member` w) . (around p2)
+    kept = S.filter ((`elem` [3,4]) . neighbours) w
+    new = S.filter ((== 3) . neighbours) . S.fromList $ concatMap (around p2) w
+    neighbours = length . filter (`S.member` w) . (around p2)
 
-around :: Bool -> Point -> [Point]
+around :: VaryW -> Point -> [Point]
 around vary_w (P w' x' y' z') = [
     P (w' + w) (x' + x) (y' + y) (z' + z) |
-    w <- if vary_w then [0] else [-1..1],
+    w <- if vary_w == ZeroW then [0] else [-1..1],
     [x, y, z] <- replicateM 3 [-1..1]
   ]
 
