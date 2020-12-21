0f54a3e4494e9dea1a72353f0e45f51093e6659f
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-21 03:48:31 +0100

    Day 19 part 2 (2020)

--- 2020/Day19.hs
+++ 2020/Day19.hs
@@ -1,29 +1,26 @@
 import Advent
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
-import Data.Foldable
 
-data Rule = Lit Char | Rules [[Int]] deriving (Show, Eq, Ord)
-type RuleMap = Map Int Rule
-type Input = (RuleMap, [String])
+data Rule = Lit Char | Or Rule Rule | And Rule Rule | See Int deriving (Show, Eq, Ord)
+type Input = (Map Int Rule, [String])
 
-main = runSoln' (parseAll pInput) part1 (const 0)
+main = runSoln' (parseAll pInput) part1 part2
 
 part1 :: Input -> Int
-part1 (rules, xs) = length $ filter (\x -> case matches rules 0 x of
-    Just "" -> True
-    _ -> False
-  ) xs
+part1 (rules, xs) = countp ((> 0) . countp null . solve rules (See 0)) xs
 
-matches :: RuleMap -> Int -> String -> Maybe String
-matches m r xs = case m M.! r of
-  Lit e -> case xs of
-    (x:xs) -> if e == x then Just xs else Nothing
-    _ -> Nothing
-  Rules opts -> asum $ map (\bs -> matchesSeq m bs xs) opts
+part2 :: Input -> Int
+part2 (rules, xs) = part1 (add "8: 42 | 42 8" $ add "11: 42 31 | 42 11 31" rules, xs)
+  where
+    add = uncurry M.insert . parseAll pPair
 
-matchesSeq :: RuleMap -> [Int] -> String -> Maybe String
-matchesSeq m rs xs = foldlM (\xs r -> matches m r xs) xs rs
+solve :: Map Int Rule -> Rule -> String -> [String]
+solve _ (Lit e) (x:xs) = if e == x then [xs] else []
+solve _ (Lit _) [] = []
+solve m (Or l r) xs = solve m l xs ++ solve m r xs
+solve m (And l r) xs = concatMap (solve m r) $ solve m l xs
+solve m (See n) xs = solve m (m M.! n) xs
 
 l :: Parser a -> Parser a
 l p = p <* hspace
@@ -39,4 +36,4 @@ pPair = (,) <$> l decimal <* l (char ':') <*> pRule
 
 pRule :: Parser Rule
 pRule = Lit <$> (try (char '"') *> letterChar <* l (char '"'))
-  <|> Rules <$> (decimal `endBy` hspace) `sepBy` l (string "|")
+  <|> foldl1' Or <$> (foldl1' And . map See <$> decimal `endBy` hspace) `sepBy` l (string "|")
