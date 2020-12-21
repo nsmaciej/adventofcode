0214cd7dfef61183dcb4394861efd8541c89a55a
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-21 19:38:14 +0100

    Day 13 part 1 (2020)

---
+++ 2020/Day13.hs
@@ -0,0 +1,17 @@
+import Advent
+import Data.Function (on)
+
+type Input = (Int, [Int])
+
+main = runSoln' (parseAll pInput) part1 id
+
+part1 :: Input -> Int
+part1 (t, xs) = bus * (next bus - t)
+  where
+    next x = x * (1 + t `div` x)
+    bus = minimumBy (compare `on` next) xs
+
+pInput :: Parser Input
+pInput = (,)
+  <$> decimal <* newline
+  <*> decimal `sepEndBy` some (oneOf "x,") <* newline
