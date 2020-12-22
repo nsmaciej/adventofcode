043a4680c0d550e89ca40ca277f95df09b35136a
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-22 08:08:14 +0100

    Day 22 (part 2)

--- 2020/Day22.hs
+++ 2020/Day22.hs
@@ -1,24 +1,52 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Advent
 import Data.Foldable (toList)
+import Data.Set (Set)
 import Data.Sequence (Seq(..), (|>))
-import qualified Data.Sequence as S
+import qualified Data.Sequence as Seq
+import qualified Data.Set as Set
 
-type Game = (Seq Int, Seq Int)
+type Deck = Seq Int
+type Game = (Deck, Deck)
+type Key = (Integer, Integer)
 
-main = runSoln' (parseAll pGame) (uncurry play) (const 0)
+main = runSoln'
+  (parseAll pGame)
+  (uncurry play) 
+  (either score score . uncurry (playRec Set.empty))
 
-play :: Seq Int -> Seq Int -> Int
+play :: Deck -> Deck -> Int
 play Empty rs = score rs
 play ls Empty = score ls
 play (l :<| ls) (r :<| rs)
   | l > r = play (ls |> l |> r) rs
   | r > l = play ls (rs |> r |> l)
 
+playRec :: Set Key -> Deck -> Deck -> Either Deck Deck 
+playRec _ Empty rs = Right rs
+playRec _ ls Empty = Left ls
+playRec seen lall@(l :<| ls) rall@(r :<| rs)
+  | Set.member (encode lall rall) seen = Left ls
+  | l <= Seq.length ls && r <= Seq.length rs =
+    case playRec Set.empty (Seq.take l ls) (Seq.take r rs) of
+      Left _ -> left
+      Right _ -> right
+  | l > r = left
+  | r > l = right
+  where
+    seen' = Set.insert (encode lall rall) seen
+    left = playRec seen' (ls |> l |> r) rs
+    right = playRec seen' ls (rs |> r |> l)
+
+encode :: Deck -> Deck -> Key
+encode l r = (go l, go r)
+  where
+    go deck = toInteger $ foldl' (\a x -> 52 * a + x) 0 deck
+
 score :: Seq Int -> Int
 score deck = sum $ zipWith (*) [1..] (reverse $ toList deck)
 
-pGame :: Parser Game
-pGame = (\x y -> (S.fromList x, S.fromList y))
+pGame :: Parser (Deck, Deck)
+pGame = (\x y -> (Seq.fromList x, Seq.fromList y))
   <$> ("Player 1:\n" *> decimal `endBy` newline <* newline)
   <*> ("Player 2:\n" *> decimal `endBy` newline)
