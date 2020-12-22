29f95a43d32a998c4beb877039cd59c63b6d8aaa
Maciej Goszczycki <mgoszcz2@gmail.com>
2020-12-22 03:06:36 +0100

    Day 11 part 2 (2020)

--- 2020/Day11.hs
+++ 2020/Day11.hs
@@ -1,25 +1,40 @@
 import Advent
-import Data.Map.Strict (Map)
+import Data.Map.Strict (Map, (!?))
 import qualified Data.Map.Strict as M
+import Data.Maybe
 
 type Point = (Int, Int)
 type Seats = Map Point Seat
-data Seat = Empty | Full deriving (Show, Eq)
-main = runSoln' parse part1 (const 0)
+data Seat = Empty | Full | Floor deriving (Show, Eq)
+type Occupied = Seats -> Point -> Int
 
-part1 :: Seats -> Int
-part1 w
+main = runSoln' parse (const 0) (solve $ step occupied' 5)
+
+infixl 6 +.
+(+.) :: Point -> Point -> Point
+(x, y) +. (x', y') = (x + x', y + y')
+
+solve :: (Seats -> Seats) -> Seats -> Int
+solve step w
   | w == step w = M.size $ M.filter (== Full) w
-  | otherwise = part1 (step w)
+  | otherwise = solve step (step w)
 
-step :: Seats -> Seats
-step w = M.mapWithKey update w
+step :: Occupied -> Int -> Seats -> Seats
+step occupied emtpy w = M.mapWithKey update w
   where
     update p Empty = if occupied w p == 0 then Full else Empty
-    update p Full = if occupied w p >= 4 then Empty else Full
+    update p Full = if occupied w p >= emtpy then Empty else Full
+    update _ Floor = Floor
 
-occupied :: Seats -> Point -> Int
-occupied w (x', y') = length [1 | x <- [-1..1], y <- [-1..1], x /= 0 || y /= 0, w M.!? (x' + x, y' + y) == Just Full]
+occupied :: Occupied
+occupied w p = length [1 | x <- [-1..1], y <- [-1..1], x /= 0 || y /= 0, w !? ((x, y) +. p) == Just Full]
+
+walk :: Seats -> Point -> Point -> Maybe Seat
+walk w p delta = fromJust $ find (/= Just Floor) . map (w !?) . tail $ iterate (+. delta) p
+
+occupied' :: Occupied
+occupied' w p = length [1 | x <- [-1..1], y <- [-1..1], x /= 0 || y /= 0, walk w p (x, y) == Just Full]
 
 parse :: String -> Seats
-parse str = M.fromList [((y, x), Empty) | (y, xs) <- zip [0..] (lines str), (x, i) <- zip [0..] xs, i == 'L']
+parse str = M.fromList [((y, x), if i == 'L' then Empty else Floor)
+  | (y, xs) <- zip [0..] (lines str), (x, i) <- zip [0..] xs]
