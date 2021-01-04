d011d81f5c1ae2218780f66a57fb82dc1cd23c41
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-01-04 09:00:11 +0100

    Day 23 part 2 - hacky unsafe mutability (2020)

--- 2020/Day23.hs
+++ 2020/Day23.hs
@@ -1,25 +1,65 @@
-import Advent
-import Data.Sequence (Seq(Empty), (><), (|>))
-import qualified Data.Sequence as S
-import Data.Foldable (toList)
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE NumericUnderscores #-}
+module Main where
+
+import Advent (runSoln', Print(..))
+import Control.Monad.ST (runST)
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as MV
+
+-- | Unboxed vector representing a linked list.
+type Cups = V.Vector Int
+-- | Current game state: the 'load'ed input and the value of the current cup.
+data Game = Game Int Cups deriving (Show)
 
 main :: IO ()
-main = runSoln' (S.fromList . map (read . pure)) (Print . part1) (const 0)
+main = runSoln' (map (read . pure)) (Print . part1) part2
 
-part1 :: Seq Int -> String
-part1 = concat . toList . fmap show . (!! 100) . iterate step
+part1 :: [Int] -> String
+part1 = concat . map show . tail . unload1 . (!! 100) . iterate step . load
 
-step :: Seq Int -> Seq Int
-step cups = (l >< pick >< r) |> current
+part2 :: [Int] -> Int
+part2 game = soln !! 1 * soln !! 2
   where
-    current = S.index cups 0
-    (pick, left) = S.splitAt 3 $ S.drop 1 cups
-    Just dest = findMaxIx (< current) left <|> findMaxIx (const True) left
-    (l, r) = S.splitAt (dest + 1) left
+    soln = unload1 . (!! 10_000_000) . iterate step . load $ game ++ [10..1000_000]
 
-findMaxIx :: Ord a => (a -> Bool) -> Seq a -> Maybe Int
-findMaxIx _ Empty = Nothing
-findMaxIx f xs = fst <$> S.foldlWithIndex go Nothing xs
+-- | Load the input into a list where each index is the element stored and the
+-- value is the index of the next element.
+load :: [Int] -> Game
+load xs = Game (head xs) $ V.replicate (1 + length xs) 0 V.// go (xs ++ [head xs])
   where
-    go Nothing i x = if f x then Just (i, x) else Nothing
-    go (Just (ti, tx)) i x = if f x && x > tx then Just (i, x) else Just (ti, tx)
+    go (x : y : xs) = (x, y) : go (y : xs)
+    go _ = []
+
+unload :: Game -> [Int]
+unload (Game current list) = go (V.length list) current
+  where
+    go 1 _ = []
+    go n k = k : go (n - 1) (list V.! k)
+
+-- | Unload, staring the cups at cup '1'.
+unload1 :: Game -> [Int]
+unload1 (Game _ list) = unload $ Game 1 list
+
+-- | Execute a single step of the game.
+step :: Game -> Game
+step (Game current list) = runST $ do
+  let next = (list V.!)
+  let pick@[!c1, _, !c3] = take 3 . tail $ iterate next current
+  let !dest = predCup (V.length list) pick current
+  let !dest' = next dest
+  let !c3' = next c3
+  mv <- V.unsafeThaw list
+  MV.unsafeWrite mv dest c1
+  MV.unsafeWrite mv c3 dest'
+  MV.unsafeWrite mv current c3'
+  list' <- V.unsafeFreeze mv
+  return $ Game (list' V.! current) list'
+
+-- | Find the index of the previous unused cup.
+predCup :: Int -> [Int] -> Int -> Int
+predCup len taken n = go n
+  where
+    go n
+      | n > 1 = if n - 1 `elem` taken then go (n - 1) else n - 1
+      | otherwise = go len
