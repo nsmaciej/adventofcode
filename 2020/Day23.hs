48364aa84b14f4689fae3e042c9aba02f5677ed2
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-01-05 19:56:24 +0100

    Optimize the heck out of day 23 (2020)
    * Using Int32 actually helps a lot
    * Never freeze and thaw the vector
    * Use 'until' which seems faster than the previous 'predCup'

--- 2020/Day23.hs
+++ 2020/Day23.hs
@@ -1,70 +1,58 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE NumericUnderscores #-}
-module Main where
-
 import Advent (runSoln', Print(..))
-import Control.Monad.ST
-import qualified Data.Vector.Unboxed as V
+import Data.Int (Int32)
+import Control.Monad (zipWithM_)
+import Control.Monad.ST (ST, runST)
 import qualified Data.Vector.Unboxed.Mutable as MV
 
--- | Mutable version of 'Game' over a PrimState.
-data MGame s = MGame Int (V.MVector s Int)
+type Cups s = MV.MVector s Int32
 
 main :: IO ()
 main = runSoln' (map (read . pure)) (Print . part1) part2
 
-part1 :: [Int] -> String
+part1 :: [Int32] -> String
 part1 = concat . map show . tail . play 100
 
-part2 :: [Int] -> Int
-part2 game = soln !! 1 * soln !! 2
+part2 :: [Int32] -> Integer
+part2 game = toInteger x * toInteger y
   where
-    soln = play 10_000_000 $ game ++ [10..1000_000]
+    (_:x:y:_) = play 10_000_000 $ game ++ [10..1000_000]
 
--- | Load the input into a list where each index is the element stored and the
--- value is the index of the next element.
-load :: [Int] -> V.Vector Int
-load xs = V.replicate (1 + length xs) 0 V.// go (xs ++ [head xs])
+load :: [Int32] -> ST s (Cups s)
+load xs = do
+  v <- MV.new (1 + length xs)
+  zipWithM_ (MV.write v . fromIntegral) xs (tail xs) -- Very neat, copied from glguy.
+  MV.write v (fromIntegral $ last xs) (head xs) -- Make it circular.
+  return v
+
+unload :: Cups s -> ST s [Int32]
+unload xs = go (MV.length xs) 1
   where
-    go (x : y : xs) = (x, y) : go (y : xs)
-    go _ = []
+    go 1 _ = return []
+    go n k = (k :) <$> (MV.read xs (fromIntegral k) >>= go (n - 1))
 
--- | Convert a 'Game' into the cup list, starting with the cup number one.
-unload :: V.Vector Int -> [Int]
-unload xs = go (V.length xs) 1
+play :: Int32 -> [Int32] -> [Int32]
+play n xs = runST $ snd <$> go (n - 1) >>= unload
   where
-    go 1 _ = []
-    go n k = k : go (n - 1) (xs V.! k)
-
-play :: Int -> [Int] -> [Int]
-play n xs = unload $ runST $ do
-  mv <- V.thaw $ load xs
-  MGame _ mv' <- iterateM step (MGame (head xs) mv) !! (n + 1)
-  V.freeze mv'
+    go 0 = (head xs,) <$> load xs
+    go n = go (n - 1) >>= step
 
 -- | Execute a single step of the game.
-step :: MGame s -> ST s (MGame s)
-step (MGame current cups) = do
+step :: (Int32, Cups s) -> ST s (Int32, Cups s)
+step (current, cups) = do
   c1 <- next current
   c2 <- next c1
   c3 <- next c2
-  c3_next <- next c3
-  let dest = predCup (MV.length cups) (c2,c3,c3) current
-  dest_next <- next dest
+  let dest = until (\x -> x /= c1 && x /= c2 && x /= c3) dec (current - 1)
+  c3' <- next c3
+  dest' <- next dest
   write dest c1
-  write c3 dest_next
-  write current c3_next
-  MGame <$> next current <*> pure cups
+  write c3 dest'
+  write current c3'
+  (,cups) <$> next current
   where
-    next = MV.read cups
-    write = MV.write cups
-
--- | Find the index of the previous unused cup.
-predCup :: Int -> (Int, Int, Int) -> Int -> Int
-predCup len (c1,c2,c3) n = go n
-  where
-    go n
-      | n > 1 = if n - 1 == c1 || n - 1 == c2 || n - 1 == c3 then go (n - 1) else n - 1
-      | otherwise = go len
-
-iterateM :: Monad m => (a -> m a) -> a -> [m a]
-iterateM f = iterate (>>= f) . return
+    next = MV.read cups . fromIntegral
+    write = MV.write cups . fromIntegral
+    dec 1 = fromIntegral $ MV.length cups - 1
+    dec k = k - 1
