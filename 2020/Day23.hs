b073521e007167fbde8cafbeb8ae51910a25d038
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-01-05 03:29:36 +0100

    Day 23 - Stay inside the ST monad for longer (2020)

--- 2020/Day23.hs
+++ 2020/Day23.hs
@@ -1,65 +1,70 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE NumericUnderscores #-}
 module Main where
 
 import Advent (runSoln', Print(..))
-import Control.Monad.ST (runST)
+import Control.Monad.ST
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as MV
 
--- | Unboxed vector representing a linked list.
-type Cups = V.Vector Int
--- | Current game state: the 'load'ed input and the value of the current cup.
-data Game = Game Int Cups deriving (Show)
+-- | Mutable version of 'Game' over a PrimState.
+data MGame s = MGame Int (V.MVector s Int)
 
 main :: IO ()
 main = runSoln' (map (read . pure)) (Print . part1) part2
 
 part1 :: [Int] -> String
-part1 = concat . map show . tail . unload1 . (!! 100) . iterate step . load
+part1 = concat . map show . tail . play 100
 
 part2 :: [Int] -> Int
 part2 game = soln !! 1 * soln !! 2
   where
-    soln = unload1 . (!! 10_000_000) . iterate step . load $ game ++ [10..1000_000]
+    soln = play 10_000_000 $ game ++ [10..1000_000]
 
 -- | Load the input into a list where each index is the element stored and the
 -- value is the index of the next element.
-load :: [Int] -> Game
-load xs = Game (head xs) $ V.replicate (1 + length xs) 0 V.// go (xs ++ [head xs])
+load :: [Int] -> V.Vector Int
+load xs = V.replicate (1 + length xs) 0 V.// go (xs ++ [head xs])
   where
     go (x : y : xs) = (x, y) : go (y : xs)
     go _ = []
 
-unload :: Game -> [Int]
-unload (Game current list) = go (V.length list) current
+-- | Convert a 'Game' into the cup list, starting with the cup number one.
+unload :: V.Vector Int -> [Int]
+unload xs = go (V.length xs) 1
   where
     go 1 _ = []
-    go n k = k : go (n - 1) (list V.! k)
+    go n k = k : go (n - 1) (xs V.! k)
 
--- | Unload, staring the cups at cup '1'.
-unload1 :: Game -> [Int]
-unload1 (Game _ list) = unload $ Game 1 list
+play :: Int -> [Int] -> [Int]
+play n xs = unload $ runST $ do
+  mv <- V.thaw $ load xs
+  MGame _ mv' <- iterateM step (MGame (head xs) mv) !! (n + 1)
+  V.freeze mv'
 
 -- | Execute a single step of the game.
-step :: Game -> Game
-step (Game current list) = runST $ do
-  let next = (list V.!)
-  let pick@[!c1, _, !c3] = take 3 . tail $ iterate next current
-  let !dest = predCup (V.length list) pick current
-  let !dest' = next dest
-  let !c3' = next c3
-  mv <- V.unsafeThaw list
-  MV.unsafeWrite mv dest c1
-  MV.unsafeWrite mv c3 dest'
-  MV.unsafeWrite mv current c3'
-  list' <- V.unsafeFreeze mv
-  return $ Game (list' V.! current) list'
+step :: MGame s -> ST s (MGame s)
+step (MGame current cups) = do
+  c1 <- next current
+  c2 <- next c1
+  c3 <- next c2
+  c3_next <- next c3
+  let dest = predCup (MV.length cups) (c2,c3,c3) current
+  dest_next <- next dest
+  write dest c1
+  write c3 dest_next
+  write current c3_next
+  MGame <$> next current <*> pure cups
+  where
+    next = MV.read cups
+    write = MV.write cups
 
 -- | Find the index of the previous unused cup.
-predCup :: Int -> [Int] -> Int -> Int
-predCup len taken n = go n
+predCup :: Int -> (Int, Int, Int) -> Int -> Int
+predCup len (c1,c2,c3) n = go n
   where
     go n
-      | n > 1 = if n - 1 `elem` taken then go (n - 1) else n - 1
+      | n > 1 = if n - 1 == c1 || n - 1 == c2 || n - 1 == c3 then go (n - 1) else n - 1
       | otherwise = go len
+
+iterateM :: Monad m => (a -> m a) -> a -> [m a]
+iterateM f = iterate (>>= f) . return
