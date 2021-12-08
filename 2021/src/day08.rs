9befc0fa1d04c57d4e0463392ca1a7e5ed197f09
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-12-08 20:39:17 +0100

    Clean up day 8 a bit (2021)

--- 2021/src/day08.rs
+++ 2021/src/day08.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::aoc::*;
 
-fn segments() -> [u8; 10] {
+fn segments(n: u8) -> u8 {
     [
         //abcdefg-
         0b11101110, // 0
@@ -15,32 +15,40 @@ fn segments() -> [u8; 10] {
         0b10100100, // 7
         0b11111110, // 8
         0b11110110, // 9
-    ]
+    ][n as usize]
 }
 
-fn assume<'a, 'b: 'a>(
-    segments: &[u8; 10],
-    cands: &mut Vec<Vec<u8>>,
-    n: u8,
-    pat: &'b str,
-    assoc: &'a mut HashMap<&'b str, u8>,
-) -> bool {
-    for k in 0..8 {
-        if (segments[n as usize].reverse_bits() >> k) & 1 > 0 {
-            cands[k].retain(|x| pat.as_bytes().contains(&(x + 'a' as u8)));
-            if cands[k].len() == 0 {
-                return false;
-            }
+#[derive(Clone, Debug)]
+struct Search<'a> {
+    pattern_digit: HashMap<&'a str, u8>,
+    segment_candidates: Vec<Vec<u8>>,
+}
+
+impl<'a> Search<'a> {
+    fn new() -> Search<'a> {
+        Search {
+            pattern_digit: HashMap::new(),
+            segment_candidates: vec![(0..8).collect(); 8],
         }
     }
-    assoc.insert(pat, n);
-    true
+
+    fn assume<'k, 'b: 'a>(&'k mut self, n: u8, pat: &'b str) -> bool {
+        for k in 0..8 {
+            if (segments(n) >> (7 - k)) & 1 > 0 {
+                self.segment_candidates[k].retain(|x| pat.as_bytes().contains(&(x + 'a' as u8)));
+                if self.segment_candidates[k].len() == 0 {
+                    return false;
+                }
+            }
+        }
+        self.pattern_digit.insert(pat, n);
+        true
+    }
 }
 
 pub fn solve(input: Vec<String>) -> (usize, u32) {
     let mut part1 = 0;
     let mut part2 = 0;
-    let segments = segments();
     for line in input {
         let (patterns, outputs) = line.split(" | ").collect_tuple().unwrap();
         let outputs = outputs
@@ -59,34 +67,27 @@ pub fn solve(input: Vec<String>) -> (usize, u32) {
             .filter(|x| *x == 2 || *x == 4 || *x == 3 || *x == 7)
             .count();
 
-        let mut candidate_orig = vec![vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 8];
-        let mut assoc: HashMap<&str, u8> = HashMap::new();
-        assume(&segments, &mut candidate_orig, 1, &pats[0], &mut assoc); // 2 on
-        assume(&segments, &mut candidate_orig, 7, &pats[1], &mut assoc); // 3 on
-        assume(&segments, &mut candidate_orig, 4, &pats[2], &mut assoc); // 4 on
-        assume(&segments, &mut candidate_orig, 8, &pats[9], &mut assoc); // 8 on
+        let mut search = Search::new();
+        search.assume(1, &pats[0]); // 2 on
+        search.assume(7, &pats[1]); // 3 on
+        search.assume(4, &pats[2]); // 4 on
+        search.assume(8, &pats[9]); // 8 on
 
-        // Which digits can go in this segment.
         'search: for (fives, sixes) in [2, 3, 5]
             .iter()
             .permutations(3)
             .cartesian_product([0, 6, 9].iter().permutations(3))
         {
-            let mut candidates = candidate_orig.clone();
-            for (i, five) in fives.iter().enumerate() {
-                if !assume(&segments, &mut candidates, **five, &pats[i + 3], &mut assoc) {
-                    continue 'search;
-                }
-            }
-            for (i, six) in sixes.iter().enumerate() {
-                if !assume(&segments, &mut candidates, **six, &pats[i + 6], &mut assoc) {
+            let mut sub_search = search.clone();
+            for (i, digit) in fives.iter().chain(sixes.iter()).enumerate() {
+                if !sub_search.assume(**digit, &pats[i + 3]) {
                     continue 'search;
                 }
             }
             let mut res: u32 = 0;
             for out in outputs {
                 res *= 10;
-                res += assoc[out.as_str()] as u32;
+                res += sub_search.pattern_digit[out.as_str()] as u32;
             }
             part2 += res;
             break;
