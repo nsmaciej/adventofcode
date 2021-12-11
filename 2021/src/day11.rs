68c69e8e7cd4c1392b33b7f117cbc8ff4f47befa
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-12-11 18:22:24 +0100

    Speed up day 11 a lot (2021)
    Mostly involves minimising recursion and keeping flashed state and energy levels together.

--- 2021/src/day11.rs
+++ 2021/src/day11.rs
@@ -1,75 +1,68 @@
 use crate::utils::Grid;
 
-fn step(grid: &mut Vec<Vec<u8>>) -> i64 {
-    let mut stack = Vec::new();
-    let mut flashed = vec![vec![false; grid.width()]; grid.height()];
-
-    for y in 0..grid.width() {
-        for x in 0..grid.height() {
-            grid[y][x] += 1;
-            if grid[y][x] > 9 {
-                stack.push((y, x));
-            }
-        }
-    }
-
-    while let Some((y, x)) = stack.pop() {
-        if flashed[y][x] {
-            continue;
-        }
-        flashed[y][x] = true;
-        macro_rules! flash {
-            ($y:expr, $x:expr) => {
-                if let Some(cell) = grid.getyx_mut($y, $x) {
-                    *cell += 1;
-                    if *cell > 9 && !flashed[$y][$x] {
-                        stack.push(($y, $x));
-                    }
-                }
-            };
-        }
-        flash!(y + 1, x);
-        flash!(y + 1, x + 1);
-        flash!(y + 1, x.overflowing_sub(1).0);
-
-        flash!(y, x + 1);
-        flash!(y, x.overflowing_sub(1).0);
-
-        flash!(y.overflowing_sub(1).0, x);
-        flash!(y.overflowing_sub(1).0, x + 1);
-        flash!(y.overflowing_sub(1).0, x.overflowing_sub(1).0);
-    }
-
-    let mut r = 0;
-    for y in 0..grid.width() {
-        for x in 0..grid.height() {
-            if flashed[y][x] {
-                r += 1;
-                grid[y][x] = 0;
-            }
-        }
-    }
-    r
+fn around(y: usize, x: usize) -> [(i64, i64); 8] {
+    let y = y as i64;
+    let x = x as i64;
+    [
+        (y - 1, x - 1),
+        (y - 1, x),
+        (y - 1, x + 1),
+        (y, x + 1),
+        (y + 1, x + 1),
+        (y + 1, x),
+        (y + 1, x - 1),
+        (y, x - 1),
+    ]
 }
 
-pub fn solve(input: String) -> (i64, i64) {
-    let mut grid: Vec<Vec<u8>> = input
-        .lines()
-        .map(|line| line.bytes().map(|x| x - b'0').collect())
-        .collect();
-
-    let mut total = 0;
-    for _ in 0..100 {
-        total += step(&mut grid);
+fn flood(grid: &mut Vec<Vec<i8>>, ys: usize, xs: usize) {
+    for (y, x) in around(ys, xs) {
+        let Some(cell) = grid.getyx_mut(y as usize, x as usize) else {
+            continue; // Out of bounds.
+        };
+        if *cell == -1 {
+            continue; // Already flashed.
+        }
+        *cell += 1;
+        if *cell > 9 {
+            *cell = -1;
+            flood(grid, y as usize, x as usize);
+        }
     }
+}
 
-    let mut i = 0;
-    loop {
-        i += 1;
-        if step(&mut grid) as usize == grid.width() * grid.height() {
-            break;
+fn step(grid: &mut Vec<Vec<i8>>) -> usize {
+    grid.iter_mut().flatten().for_each(|x| *x += 1);
+
+    for y in 0..grid.width() {
+        for x in 0..grid.height() {
+            if grid[y][x] > 9 && grid[y][x] != -1 {
+                grid[y][x] = -1; // Flash!
+                flood(grid, y, x);
+            }
         }
     }
 
-    (total, 100 + i)
+    grid.iter_mut()
+        .flatten()
+        .filter(|x| **x == -1)
+        .map(|x| *x = 0)
+        .count()
+}
+
+pub fn solve(input: String) -> (usize, usize) {
+    let mut grid: Vec<Vec<i8>> = input
+        .lines()
+        .map(|line| line.bytes().map(|x| x as i8 - b'0' as i8).collect())
+        .collect();
+
+    let part1 = (0..100).map(|_| step(&mut grid)).sum();
+
+    let mut sync_step = 101;
+    let goal = grid.width() * grid.height();
+    while step(&mut grid) as usize != goal {
+        sync_step += 1;
+    }
+
+    (part1, sync_step)
 }
