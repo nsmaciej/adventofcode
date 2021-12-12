59c1d0fc7451ba755f32d89d9597a312c7f987a9
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-12-12 10:32:24 +0100

    Faster still day 12 (2021)

--- 2021/src/day12.rs
+++ 2021/src/day12.rs
@@ -1,33 +1,47 @@
 //! Passage Pathing
 
 // This code encodes "small" caves as negative numbers and "large" caves as
-// positive numbers. We use the fact that there are very few nodes to encode the
-// visit state into a u32 integer.
+// positive numbers. This is fast and also allows us to process the caves in
+// batches by sorting the adjacency list. We also use the fact that there are
+// very few nodes to encode the visit state into a u32 integer.
 
 use itertools::Itertools;
 use std::collections::BTreeMap;
 
-// These are arbitary (but compatible with the string interning mechanism).
 const START: i8 = 1;
 const END: i8 = 0;
 
-fn visit(graph: &[Vec<i8>], visited: u32, node: i8, allow_twice: bool) -> i64 {
-    graph[node.abs() as usize]
-        .iter()
-        .cloned()
-        .map(|adj| {
-            let seen = visited & (1 << adj.abs()) > 0;
-            if adj < 0 && !(seen && allow_twice) {
-                visit(graph, visited | (1 << adj.abs()), adj, seen || allow_twice)
-            } else if adj > 0 {
-                visit(graph, visited, adj, allow_twice)
-            } else if adj == END {
-                1
-            } else {
-                0
-            }
-        })
-        .sum()
+fn visit(graph: &[Vec<i8>], visited: u32, node: i8, visited_twice: bool) -> i64 {
+    let edges = &graph[node.abs() as usize];
+    let mut i = 0;
+    let mut sum = 0;
+
+    // Small caves.
+    while i < edges.len() && edges[i] < 0 {
+        let seen = visited & (1 << edges[i].abs()) > 0;
+        if !seen || !visited_twice {
+            sum += visit(
+                graph,
+                visited | (1 << edges[i].abs()),
+                edges[i],
+                seen || visited_twice,
+            );
+        }
+        i += 1;
+    }
+
+    // End.
+    if edges.get(i) == Some(&END) {
+        sum += 1;
+        i += 1;
+    }
+
+    // Big caves.
+    while i < edges.len() {
+        sum += visit(graph, visited, edges[i], visited_twice);
+        i += 1;
+    }
+    sum
 }
 
 pub fn solve(input: Vec<String>) -> (i64, i64) {
@@ -52,6 +66,9 @@ pub fn solve(input: Vec<String>) -> (i64, i64) {
         if a != START {
             graph[b.abs() as usize].push(a);
         }
+    }
+    for node in 0..strings.len() {
+        graph[node].sort_unstable();
     }
 
     (
