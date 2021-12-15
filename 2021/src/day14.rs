7a9cd4be2ce04844a08eecdca89bb5580069a2d8
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-12-15 06:31:30 +0100

    Faster day 14 (2021)

--- 2021/src/day14.rs
+++ 2021/src/day14.rs
@@ -1,29 +1,41 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 use itertools::Itertools;
+
+#[inline]
+fn key(a: u8, b: u8) -> u16 {
+    (b as u16) << 8 | a as u16
+}
+
+#[inline]
+fn unkey(ab: u16) -> (u8, u8) {
+    (ab as u8, (ab >> 8) as u8)
+}
 
 fn simulate(
     k: usize,
     first: u8,
     last: u8,
-    pairs_orig: &mut HashMap<(u8, u8), u64>,
-    rules: &HashMap<(u8, u8), u8>,
+    pairs_orig: &mut BTreeMap<u16, u64>,
+    rules: &BTreeMap<u16, u8>,
 ) -> u64 {
     let mut pairs = pairs_orig.clone();
     for _ in 0..k {
-        let mut next = HashMap::new();
-        for ((a, b), k) in pairs {
-            let result = rules[&(a, b)];
-            *next.entry((a, result)).or_default() += k;
-            *next.entry((result, b)).or_default() += k;
+        let mut next = BTreeMap::new();
+        for (ab, k) in pairs {
+            let (a, b) = unkey(ab);
+            let result = rules[&ab];
+            *next.entry(key(a, result)).or_default() += k;
+            *next.entry(key(result, b)).or_default() += k;
         }
         pairs = next;
     }
 
-    let mut counts: HashMap<u8, u64> = HashMap::new();
-    for ((a, b), k) in &pairs {
-        *counts.entry(*a).or_default() += k;
-        *counts.entry(*b).or_default() += k;
+    let mut counts: BTreeMap<u8, u64> = BTreeMap::new();
+    for (ab, k) in &pairs {
+        let (a, b) = unkey(*ab);
+        *counts.entry(a).or_default() += k;
+        *counts.entry(b).or_default() += k;
     }
 
     let (most_el, most_k) = counts.iter().max_by_key(|x| x.1).unwrap();
@@ -36,19 +48,19 @@ fn simulate(
 
 pub fn solve(input: String) -> (u64, u64) {
     let mut lines = input.lines();
-    let mut pairs: HashMap<(u8, u8), u64> = HashMap::new();
+    let mut pairs: BTreeMap<u16, u64> = BTreeMap::new();
     let template = lines.next().unwrap().bytes().collect_vec();
     let first = *template.first().unwrap();
     let last = *template.first().unwrap();
     for (a, b) in template.iter().tuple_windows() {
-        *pairs.entry((*a, *b)).or_default() += 1;
+        *pairs.entry(key(*a, *b)).or_default() += 1;
     }
     lines.next(); // Skip the blank line.
-    let rules: HashMap<(u8, u8), u8> = lines
+    let rules: BTreeMap<u16, u8> = lines
         .map(|rule| {
             let (pair, result) = rule.split(" -> ").collect_tuple().unwrap();
             let (a, b) = pair.bytes().collect_tuple().unwrap();
-            ((a, b), result.bytes().next().unwrap())
+            (key(a, b), result.bytes().next().unwrap())
         })
         .collect();
 
