6352c9e4770e62eef844604ef07866525e16082f
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-12-16 05:40:38 +0100

    Much much faster day 8 (2021)

--- 2021/src/day08.rs
+++ 2021/src/day08.rs
@@ -1,99 +1,43 @@
 //! Seven Segment Search
 
-use std::collections::HashMap;
-
-use crate::utils::Itertools;
-
-fn segments(n: u8) -> u8 {
-    [
-        //abcdefg-
-        0b11101110, // 0
-        0b00100100, // 1
-        0b10111010, // 2
-        0b10110100, // 3
-        0b01110100, // 4
-        0b11010110, // 5
-        0b11011110, // 6
-        0b10100100, // 7
-        0b11111110, // 8
-        0b11110110, // 9
-    ][n as usize]
-}
-
-#[derive(Clone, Debug)]
-struct Search<'a> {
-    pattern_digit: HashMap<&'a str, u8>,
-    segment_candidates: Vec<Vec<u8>>,
-}
-
-impl<'a> Search<'a> {
-    fn new() -> Search<'a> {
-        Search {
-            pattern_digit: HashMap::new(),
-            segment_candidates: vec![(0..8).collect(); 8],
-        }
-    }
-
-    fn assume<'b: 'a>(&mut self, n: u8, pat: &'b str) -> bool {
-        for k in 0..8 {
-            if (segments(n) >> (7 - k)) & 1 > 0 {
-                self.segment_candidates[k].retain(|x| pat.as_bytes().contains(&(x + 'a' as u8)));
-                if self.segment_candidates[k].len() == 0 {
-                    return false;
-                }
-            }
-        }
-        self.pattern_digit.insert(pat, n);
-        true
+fn deduce_digit(counts: &[u8; 7], pattern: &str) -> usize {
+    // Sum of the frequencies of charcaters in each pattern is unique per digit.
+    // Thanks Reddit.
+    let sum = pattern.bytes().map(|x| counts[(x - b'a') as usize]).sum();
+    match sum {
+        42 => 0,
+        17 => 1,
+        34 => 2,
+        39 => 3,
+        30 => 4,
+        37 => 5,
+        41 => 6,
+        25 => 7,
+        49 => 8,
+        45 => 9,
+        _ => panic!("invalid pattern"),
     }
 }
 
-pub fn solve(input: Vec<String>) -> (usize, u32) {
+pub fn solve(input: String) -> (usize, usize) {
     let mut part1 = 0;
     let mut part2 = 0;
-    for line in input {
-        let (patterns, outputs) = line.split(" | ").collect_tuple().unwrap();
-        let outputs = outputs
-            .split_whitespace()
-            .map(|x| x.chars().sorted().collect::<String>())
-            .collect_vec();
-        let pats = patterns
-            .split_whitespace()
-            .map(|x| x.chars().sorted().collect::<String>())
-            .sorted_by_key(|x| x.len())
-            .collect_vec();
 
-        part1 += outputs
-            .iter()
-            .map(|x| x.len())
-            .filter(|x| *x == 2 || *x == 4 || *x == 3 || *x == 7)
-            .count();
+    for line in input.lines() {
+        let (patterns, outputs) = line.split_once(" | ").unwrap();
+        let mut counts = [0; 7];
+        patterns
+            .bytes()
+            .filter(|x| *x != b' ')
+            .for_each(|x| counts[(x - b'a') as usize] += 1);
 
-        let mut search = Search::new();
-        search.assume(1, &pats[0]); // 2 on
-        search.assume(7, &pats[1]); // 3 on
-        search.assume(4, &pats[2]); // 4 on
-        search.assume(8, &pats[9]); // 8 on
+        let out: Vec<usize> = outputs
+            .split_ascii_whitespace()
+            .map(|x| deduce_digit(&counts, &x))
+            .collect();
 
-        'search: for (fives, sixes) in [2, 3, 5]
-            .iter()
-            .permutations(3)
-            .cartesian_product([0, 6, 9].iter().permutations(3))
-        {
-            let mut sub_search = search.clone();
-            for (i, digit) in fives.iter().chain(sixes.iter()).enumerate() {
-                if !sub_search.assume(**digit, &pats[i + 3]) {
-                    continue 'search;
-                }
-            }
-            let mut res: u32 = 0;
-            for out in outputs {
-                res *= 10;
-                res += sub_search.pattern_digit[out.as_str()] as u32;
-            }
-            part2 += res;
-            break;
-        }
+        part1 += out.iter().filter(|x| matches!(x, 1 | 4 | 7 | 8)).count();
+        part2 += out[0] * 1000 + out[1] * 100 + out[2] * 10 + out[3];
     }
 
     (part1, part2)
