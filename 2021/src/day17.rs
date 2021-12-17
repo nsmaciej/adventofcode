cd6cef5add9f135c812272de869d4d202af3edd6
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-12-17 08:51:13 +0100

    More math day 17 (2021)

--- 2021/src/day17.rs
+++ 2021/src/day17.rs
@@ -4,23 +4,19 @@ fn parse_range(text: &str) -> (i32, i32) {
     (start.parse().unwrap(), end.parse::<i32>().unwrap())
 }
 
-fn simulate(x1: i32, x2: i32, y1: i32, y2: i32, mut dx: i32, mut dy: i32) -> Option<i32> {
+fn simulate(x1: i32, x2: i32, y1: i32, y2: i32, mut dx: i32, mut dy: i32) -> bool {
     let mut x = 0;
     let mut y = 0;
-    let mut peak = 0;
     while x <= x2 && y >= y1 {
-        if dy == 0 {
-            peak = y;
-        }
-        if x >= x1 && x <= x2 && y <= y2 && y >= y1 {
-            return Some(peak);
+        if x >= x1 && y <= y2 {
+            return true;
         }
         x += dx;
         y += dy;
         dx = 0.max(dx - 1);
         dy -= 1;
     }
-    None
+    false
 }
 
 pub fn solve(input: String) -> (i32, i32) {
@@ -28,21 +24,17 @@ pub fn solve(input: String) -> (i32, i32) {
     let (xs, ys) = data.split_once(", ").unwrap();
     let ((x1, x2), (y1, y2)) = (parse_range(xs), parse_range(ys));
 
-    let mut best = 0;
-    let mut hit = 0;
     // Quadratic to solve which triangular number will reach the target.
     let min_x = (1 + (1. + 8. * x1 as f32).sqrt() as i32) / 2;
-    for dx in min_x..=(1 + x2 / 2) {
-        for dy in y1..=y1.abs() {
-            if let Some(r) = simulate(x1, x2, y1, y2, dx, dy) {
-                hit += 1;
-                best = best.max(r);
-            }
-        }
-    }
-    // If we shoot directly at the platform we only get one chance.
-    // This assumes the platform width is smaller than it's x1 - 1.
-    let trival_y = 1 + y2 - y1;
-    let trival_x = 1 + x2 - x1;
-    (best, hit + trival_y * trival_x)
+    let easy_hits = (min_x..=(1 + x2 / 2))
+        .flat_map(|dx| (y1..-y1).filter(move |dy| simulate(x1, x2, y1, y2, dx, *dy)))
+        .count() as i32;
+
+    (
+        // The x for this doesn't matter unless it's really high.
+        (-y1 * (-y1 - 1)) / 2,
+        // If we shoot directly at the platform we only get one chance.
+        // This assumes the platform width is smaller than it's x1 - 1.
+        easy_hits + (1 + y2 - y1) * (1 + x2 - x1),
+    )
 }
