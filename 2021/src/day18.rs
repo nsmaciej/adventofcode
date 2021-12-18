ba7e49c2e1f0046190760fc8a7a89af5d990978a
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-12-18 19:53:13 +0100

    Simpler day 18 with UNPAIR (2021)

--- 2021/src/day18.rs
+++ 2021/src/day18.rs
@@ -1,77 +1,71 @@
 use itertools::Itertools;
 
 const PAIR: i32 = -1;
+const UNPAIR: i32 = -2;
 
 fn parse_sfn(line: &str) -> Vec<i32> {
     line.bytes()
         .flat_map(|x| match x {
             b'[' => Some(PAIR),
+            b']' => Some(UNPAIR),
             b'0'..=b'9' => Some((x - b'0') as i32),
-            b']' | b',' | b' ' => None,
+            b',' | b' ' => None,
             _ => panic!("unexpected character"),
         })
         .collect()
 }
 
 fn explode_sfn(sfn: &mut Vec<i32>) -> bool {
-    let mut pairs: u8 = 1;
+    let mut depth = 0;
     for i in 0..sfn.len() {
-        if sfn[i] != PAIR {
-            pairs = pairs >> pairs.trailing_zeros() & 0xfe;
-            continue;
-        }
-        pairs = pairs << 1 | 1;
-        if pairs.leading_zeros() == 2 {
+        if sfn[i] == PAIR && depth >= 4 {
             let a = sfn[i + 1];
             let b = sfn[i + 2];
-            sfn.splice(i..i + 3, [0]);
-            if let Some(x) = sfn[..i].iter_mut().rev().find(|x| **x != PAIR) {
+            sfn.splice(i..i + 4, [0]);
+            if let Some(x) = sfn[..i].iter_mut().rev().find(|x| **x >= 0) {
                 *x += a;
             }
-            if let Some(x) = sfn[i + 1..].iter_mut().find(|x| **x != PAIR) {
+            if let Some(x) = sfn[i + 1..].iter_mut().find(|x| **x >= 0) {
                 *x += b;
             }
             return true;
+        } else if sfn[i] == PAIR {
+            depth += 1;
+        } else if sfn[i] == UNPAIR {
+            depth -= 1;
         }
     }
     false
 }
 
 fn split_sfn(sfn: &mut Vec<i32>) -> bool {
-    for i in 0..sfn.len() {
-        if sfn[i] >= 10 {
-            sfn.splice(i..i + 1, [PAIR, sfn[i] / 2, (sfn[i] + 1) / 2]);
-            return true;
-        }
+    if let Some((i, x)) = sfn.iter().cloned().find_position(|x| *x >= 10) {
+        sfn.splice(i..i + 1, [PAIR, x / 2, (x + 1) / 2, UNPAIR]);
+        return true;
     }
     false
 }
 
 fn add_sfn(mut dest: Vec<i32>, rhs: &[i32]) -> Vec<i32> {
-    dest.reserve(1 + rhs.len());
+    dest.reserve(2 + rhs.len());
     dest.insert(0, PAIR);
-    dest.extend(rhs);
+    dest.extend(rhs.iter().cloned().chain([UNPAIR]));
     while explode_sfn(&mut dest) || split_sfn(&mut dest) {}
     dest
 }
 
 fn magnitude_sfn(sfn: &[i32]) -> i32 {
-    let mut pairs: u8 = 1;
     let mut mags = [0; 5];
     let mut i = 0;
     for x in sfn {
-        if *x == PAIR {
-            pairs = pairs << 1 | 1;
-            continue;
-        }
-        mags[i] = *x;
-        i += 1;
-        while pairs & 1 == 0 {
+        if *x == UNPAIR {
             mags[i - 2] = 3 * mags[i - 2] + 2 * mags[i - 1];
-            pairs >>= 1;
             i -= 1;
+            continue;
+        } else if *x >= 0 {
+            mags[i] = *x;
+            i += 1;
         }
-        pairs &= 0xfe;
     }
     mags[i - 1]
 }
