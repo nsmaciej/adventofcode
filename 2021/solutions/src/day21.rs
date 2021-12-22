aefd621f52c10850a694d9bf0fac630a87543779
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-12-22 01:33:01 +0100

    Faster and neater day 21 (2021)

--- 2021/solutions/src/day21.rs
+++ 2021/solutions/src/day21.rs
@@ -4,7 +4,7 @@ use ahash::AHashMap;
 use itertools::Itertools;
 use std::mem::swap;
 
-fn part1(mut x: i32, mut y: i32) -> i32 {
+fn part1(mut x: u64, mut y: u64) -> u64 {
     let mut x_score = 0;
     let mut y_score = 0;
     let mut rolls = 1;
@@ -20,44 +20,48 @@ fn part1(mut x: i32, mut y: i32) -> i32 {
 }
 
 // Sum of three rolls and how often it will happen per turn.
-static OUTCOMES: [(i32, i64); 7] = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)];
+static OUTCOMES: [(u64, i64); 7] = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)];
 
-type Cache = AHashMap<(i32, i32, i32, i32, bool), i64>;
+type Cache = AHashMap<u64, (i64, i64)>;
 
-fn part2(cache: &mut Cache, x: i32, x_score: i32, y: i32, y_score: i32, turn: bool) -> i64 {
-    if let Some(r) = cache.get(&(x, x_score, y, y_score, turn)) {
+fn part2(cache: &mut Cache, x: u64, x_score: u64, y: u64, y_score: u64, turn: bool) -> (i64, i64) {
+    // Save some time hashing.
+    let key = x << 24 | x_score << 16 | y << 8 | y_score << 1 | (turn as u64);
+    if let Some(r) = cache.get(&key) {
         return *r;
     }
-    let r = if x_score >= 21 {
-        return 1;
-    } else if y_score >= 21 {
-        return 0;
+
+    let wins = if y_score >= 21 {
+        (0, 1)
+    } else if x_score >= 21 {
+        (1, 0)
     } else {
-        let mut r = 0;
+        let mut wins = (0, 0);
         for (roll_sum, freq) in OUTCOMES {
-            if turn {
+            let future_wins = if turn {
                 let next_x = (x + roll_sum - 1) % 10 + 1;
-                r += freq * part2(cache, next_x, x_score + next_x, y, y_score, !turn);
+                part2(cache, next_x, next_x + x_score, y, y_score, !turn)
             } else {
                 let next_y = (y + roll_sum - 1) % 10 + 1;
-                r += freq * part2(cache, x, x_score, next_y, y_score + next_y, !turn);
-            }
+                part2(cache, x, x_score, next_y, next_y + y_score, !turn)
+            };
+            wins.0 += freq * future_wins.0;
+            wins.1 += freq * future_wins.1;
         }
-        r
+        wins
     };
-    cache.insert((x, x_score, y, y_score, turn), r);
-    r
+    cache.insert(key, wins);
+    wins
 }
 
-pub fn solve(input: String) -> (i32, i64) {
+pub fn solve(input: String) -> (u64, i64) {
     let (x, y) = input
         .lines()
         .map(|x| x.split_once(": ").unwrap().1.parse().unwrap())
         .collect_tuple()
         .unwrap();
 
-    let mut cache = Cache::new();
-    let p1 = part2(&mut cache, x, 0, y, 0, true);
-    let p2 = part2(&mut cache, y, 0, x, 0, false);
-    (part1(x, y), p1.max(p2))
+    let part1 = part1(x as u64, y as u64);
+    let part2 = part2(&mut Cache::new(), x, 0, y, 0, true);
+    (part1, part2.0.max(part2.1))
 }
