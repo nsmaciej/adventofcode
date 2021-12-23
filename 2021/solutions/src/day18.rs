a6f1ce40984bfe744c1525c29b904cda812a3399
Maciej Goszczycki <mgoszcz2@gmail.com>
2021-12-23 03:58:05 +0100

    Switch to depth + value approach for day 18 (2021)
    Around 2.5x faster

--- 2021/solutions/src/day18.rs
+++ 2021/solutions/src/day18.rs
@@ -1,85 +1,91 @@
-use itertools::Itertools;
+//! Snailfish
 
-const PAIR: i32 = -1;
-const UNPAIR: i32 = -2;
+type Sfn = Vec<(u8, u8)>;
 
-fn parse_sfn(line: &str) -> Vec<i32> {
-    line.bytes()
-        .flat_map(|x| match x {
-            b'[' => Some(PAIR),
-            b']' => Some(UNPAIR),
-            b'0'..=b'9' => Some((x - b'0') as i32),
-            b',' | b' ' => None,
+fn parse_sfn(line: &str) -> Sfn {
+    let mut depth: u8 = 0;
+    let mut r = Sfn::new();
+    for x in line.bytes() {
+        match x {
+            b'[' => depth += 1,
+            b']' => depth -= 1,
+            b'0'..=b'9' => r.push(((x - b'0') as u8, depth)),
+            b',' | b' ' => {}
             _ => panic!("unexpected character"),
-        })
-        .collect()
+        };
+    }
+    r
 }
 
-fn explode_sfn(sfn: &mut Vec<i32>) -> bool {
-    let mut depth = 0;
-    for i in 0..sfn.len() {
-        if sfn[i] == PAIR && depth >= 4 {
-            let a = sfn[i + 1];
-            let b = sfn[i + 2];
-            sfn.splice(i..i + 4, [0]);
-            if let Some(x) = sfn[..i].iter_mut().rev().find(|x| **x >= 0) {
-                *x += a;
+fn explode_sfn(sfn: &mut Sfn) -> bool {
+    for i in 0..sfn.len() - 1 {
+        let (a, depth) = sfn[i];
+        if depth >= 5 {
+            let (b, _) = sfn[i + 1];
+            sfn.splice(i..i + 2, [(0, depth - 1)]);
+            if let Some(x) = sfn.get_mut(i.overflowing_sub(1).0) {
+                x.0 += a;
             }
-            if let Some(x) = sfn[i + 1..].iter_mut().find(|x| **x >= 0) {
-                *x += b;
+            if let Some(x) = sfn.get_mut(i + 1) {
+                x.0 += b;
             }
             return true;
-        } else if sfn[i] == PAIR {
-            depth += 1;
-        } else if sfn[i] == UNPAIR {
-            depth -= 1;
+        };
+    }
+    false
+}
+
+fn split_sfn(sfn: &mut Sfn) -> bool {
+    for (i, (x, depth)) in sfn.iter().cloned().enumerate() {
+        if x >= 10 {
+            sfn.splice(i..i + 1, [(x / 2, depth + 1), ((x + 1) / 2, depth + 1)]);
+            return true;
         }
     }
     false
 }
 
-fn split_sfn(sfn: &mut Vec<i32>) -> bool {
-    if let Some((i, x)) = sfn.iter().cloned().find_position(|x| *x >= 10) {
-        sfn.splice(i..i + 1, [PAIR, x / 2, (x + 1) / 2, UNPAIR]);
-        return true;
-    }
-    false
-}
-
-fn add_sfn(mut dest: Vec<i32>, rhs: &[i32]) -> Vec<i32> {
-    dest.reserve(2 + rhs.len());
-    dest.insert(0, PAIR);
-    dest.extend(rhs.iter().cloned().chain([UNPAIR]));
+fn add_sfn(mut dest: Sfn, rhs: &[(u8, u8)]) -> Sfn {
+    dest.extend_from_slice(rhs);
+    dest.iter_mut().for_each(|(_, d)| *d += 1);
     while explode_sfn(&mut dest) || split_sfn(&mut dest) {}
     dest
 }
 
-fn magnitude_sfn(sfn: &[i32]) -> i32 {
-    let mut mags = [0; 5];
-    let mut i = 0;
-    for x in sfn {
-        if *x == UNPAIR {
-            mags[i - 2] = 3 * mags[i - 2] + 2 * mags[i - 1];
-            i -= 1;
-            continue;
-        } else if *x >= 0 {
-            mags[i] = *x;
-            i += 1;
-        }
+fn magnitude_sfn(sfn: Sfn) -> i32 {
+    fn calculate(sfn: &[(u8, u8)], depth: u8, i: &mut usize) -> i32 {
+        let a = if sfn[*i].1 == depth {
+            *i += 1;
+            sfn[*i - 1].0 as i32
+        } else {
+            calculate(sfn, depth + 1, i)
+        };
+        let b = if sfn[*i].1 == depth {
+            *i += 1;
+            sfn[*i - 1].0 as i32
+        } else {
+            calculate(sfn, depth + 1, i)
+        };
+
+        3 * a + 2 * b
     }
-    mags[i - 1]
+    calculate(&sfn, 1, &mut 0)
 }
 
 pub fn solve(input: String) -> (i32, i32) {
-    let nums: Vec<Vec<i32>> = input.lines().map(parse_sfn).collect();
+    let nums: Vec<Sfn> = input.lines().map(parse_sfn).collect();
 
-    let part1 = magnitude_sfn(&nums.iter().fold(nums[0].clone(), |a, x| add_sfn(a, x)));
-    let part2 = nums
-        .iter()
-        .permutations(2)
-        .map(|xs| magnitude_sfn(&add_sfn(xs[0].clone(), &xs[1])))
-        .max()
-        .unwrap();
+    let part1 = magnitude_sfn(nums[1..].iter().fold(nums[0].clone(), |a, x| add_sfn(a, x)));
+    let mut part2 = 0;
+    // N.B. snailfish number addition is not commutative.
+    for i in 0..nums.len() {
+        for j in 0..nums.len() {
+            if i == j {
+                continue;
+            };
+            part2 = part2.max(magnitude_sfn(add_sfn(nums[i].clone(), &nums[j])));
+        }
+    }
 
     (part1, part2)
 }
