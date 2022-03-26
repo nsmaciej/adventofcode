1dfb6f262ad0f254033bbaa04770106584014374
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-03-26 01:40:05 +0100

    Remaster day 15 (2018)

--- 2018/day15.py
+++ 2018/day15.py
@@ -2,98 +2,88 @@ import numpy as np
 from collections import deque
 
 
-def spread(m):
-    r = np.roll(m, 1, 0) | np.roll(m, -1, 0)
-    r |= np.roll(m, 1, 1) | np.roll(m, -1, 1)
+def adjacent_cells_mask(m):
+    r = np.roll(m, 1, 0) | np.roll(m, -1, 0) | np.roll(m, 1, 1) | np.roll(m, -1, 1)
     return r & ~m
 
 
-def adjacent(pt):
+def adjacent_points(pt):
     # Returned in read order.
     return ((pt[0] + y, pt[1] + x) for y, x in [(-1, 0), (0, -1), (0, 1), (1, 0)])
 
 
-def board_select(board, pt, fn):
+def find_nearest_filter(board, pt, predicate):
+    inf = 0xFFFF
     queue = deque([(0, pt)])
-    visited = np.zeros(board.shape, bool)
+    visited = np.zeros_like(board, bool)
     visited[pt] = True
-    selected = (INF, pt)
+    selected = (inf, pt)
     while queue:
         dist, pos = queue.popleft()
-        if board[pos] == "." and fn(pos):
-            selected = min(selected, (dist, pos))
+        if predicate(pos):
+            selected = min(selected, (dist, pos))  # Break ties using reading order.
         if dist >= selected[0]:
-            continue  # No point checking further.
-        for adj in adjacent(pos):
+            continue  # We will never find a better dist.
+        for adj in adjacent_points(pos):
             if board[adj] == "." and not visited[adj]:
                 visited[adj] = True
                 queue.append((1 + dist, adj))
-    if fn(selected[1]):
+    if selected[0] != inf:
         return selected[1]
     return None
 
 
-def choose_step(board, pt):
-    enemy = ENEMY[board[pt]]
-    if any(board[x] == enemy for x in adjacent(pt)):
-        # Already near enemy, board_select would fail since we occupy the spot.
+def find_next_step(board, pt, enemy):
+    if any(board[x] == enemy for x in adjacent_points(pt)):
+        # Already near enemy, since pt is not empty, code below would skip it.
         return None
-    in_range = spread(board == enemy)
-    target = board_select(board, pt, lambda x: in_range[x])
-    if target is None:
-        return None  # No reachable target.
-    valid_moves = set(adjacent(pt))
-    return board_select(board, target, lambda x: x in valid_moves)
+    in_range = adjacent_cells_mask(board == enemy)
+    if enemy := find_nearest_filter(board, pt, lambda x: in_range[x]):
+        valid_moves = set(adjacent_points(pt))
+        return find_nearest_filter(board, enemy, lambda x: x in valid_moves)
+    return None  # No reachable target.
 
 
-def entities(board):
-    return np.isin(board, list("EG"))
+def units_mask(board):
+    return np.isin(board, ["E", "G"], assume_unique=True)  # Returns reading order.
 
 
 def solve(board, elf_attack):
-    hp = np.zeros(board.shape, int)
-    hp[entities(board)] = 200
+    enemy_for_unit = {"E": "G", "G": "E"}
+    hp = np.zeros_like(board, int)
+    hp[units_mask(board)] = 200
     turn = 0
-    alive = {"E": (board == "E").sum(), "G": (board == "G").sum()}
-    deaths = {"E": 0, "G": 0}
+    alive = {"E": np.count_nonzero(board == "E"), "G": np.count_nonzero(board == "G")}
     while True:
-        order = list(map(tuple, np.argwhere(entities(board))))
-        for pos in order:
+        for pos in map(tuple, np.argwhere(units_mask(board))):
             if board[pos] == ".":
-                continue  # Got killed.
-            enemy = ENEMY[board[pos]]
-            step = choose_step(board, pos)
-            if step is not None:
+                continue  # Was alive during argwhere, but got killed.
+            enemy = enemy_for_unit[board[pos]]
+            if step := find_next_step(board, pos, enemy):  # Consider moving.
                 board[step], board[pos] = board[pos], "."
-                hp[step] = hp[pos]
+                hp[step], hp[pos] = hp[pos], 0
                 pos = step
-            # Attack
-            attack = min(
-                (x for x in adjacent(pos) if board[x] == enemy),
-                key=lambda x: hp[x],
-                default=None,
-            )
-            if attack is not None:
-                hp[attack] -= elf_attack if board[pos] == "E" else 3
-                if hp[attack] <= 0:
-                    deaths[enemy] += 1
-                    board[attack] = "."
-        if alive["E"] == deaths["E"] or alive["G"] == deaths["G"]:
-            return hp[entities(board)].sum() * turn, deaths
+            nearby_enemies = (x for x in adjacent_points(pos) if board[x] == enemy)
+            if target := min(nearby_enemies, key=lambda x: hp[x], default=None):
+                hp[target] -= elf_attack if board[pos] == "E" else 3
+                if hp[target] <= 0:
+                    board[target] = "."
+                    hp[target] = 0
+                    alive[enemy] -= 1
+        if alive["E"] == 0 or alive["G"] == 0:
+            return hp.sum() * turn, alive
         turn += 1
 
 
-PRINT = False
-INF = 0xFFFF
-ENEMY = {"E": "G", "G": "E"}
 board = np.array([list(x.strip()) for x in open("inputs/day15.txt")])
 
 print(solve(board.copy(), 3)[0])
 
 elf_attack = 4
+alive_elfs = np.count_nonzero(board == "E")
 while True:
-    answer, deaths = solve(board.copy(), elf_attack)
-    if deaths["E"] == 0:
+    answer, alive = solve(board.copy(), elf_attack)
+    if alive["E"] == alive_elfs:
         print(answer)
         break
     elf_attack += 1
