2bbe1de5b3b2a39fc32cd88bbc080f7df0f73421
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-12-08 21:11:12 +0100

    Day 8 - Clean up (2022)

--- 2022/aoc/day08.clj
+++ 2022/aoc/day08.clj
@@ -2,48 +2,43 @@
   (:require [clojure.string :as str]
             [aoc.utils :as u]))
 
-(defn- visible [xs]
+(defn- visible-ground
+  "Looking right, return true for each treetop that
+   would be visible from the ground."
+  [xs]
   (map > xs (reductions max -1 xs)))
 
-(defn- visible-both [xs]
-  (map #(or %1 %2) (-> xs reverse visible reverse) (visible xs)))
-
-(defn- visible-at-k [k xs]
+(defn- visible-at-k
+  "Looking left, return how many treetops we would see below
+   k height, up the to the first treetop at exactly k."
+  [k xs]
   (->> xs
        (partition 2 1)
        (reductions #(if (>= (first %2) k) 1 (inc %1)) 0)))
 
-(defn- visible-at-k-both [k xs]
-  (map *
-       (visible-at-k k xs)
-       (->> xs reverse (visible-at-k k) reverse)))
-
-(defn- visible-at-k-grid [k grid]
-  (let [left-right (map #(visible-at-k-both k %) grid)
-        top-down (->> grid
-                      u/transpose-grid
-                      (map #(visible-at-k-both k %))
-                      u/transpose-grid)]
-    (u/map-grid * top-down left-right)))
+(defn- omni-map
+  "Carefully apply transposes and reverses to apply a function that
+   operates on a single row, looking left, to an entire grid."
+  [combine f grid]
+  (let [bidirectional-f #(map combine (f %) (-> % reverse f reverse))
+        left-right (map bidirectional-f grid)
+        top-down (->> grid u/transpose (map bidirectional-f) u/transpose)]
+    (u/map-grid combine top-down left-right)))
 
 (defn- parse [input]
   (u/mapv-grid #(- (int %) 48) (str/split-lines input)))
 
 (defn- part1 [data]
-  (let [left-right (map visible-both data)
-        top-down (->> data
-                      u/transpose-grid
-                      (map visible-both)
-                      u/transpose-grid)]
-    (->> (u/map-grid #(or %1 %2) top-down left-right)
-         (u/filter-grid identity)
-         count)))
+  (->> (omni-map #(or %1 %2) visible-ground data)
+       flatten
+       (filter identity)
+       count))
 
 (defn- part2 [data]
-  (->> (map #(visible-at-k-grid % data) (range 10))
-       (apply u/map-grid #(nth %& %) data)
-       flatten
-       (apply max)))
+  (let [cache (map (fn [k] (omni-map * #(visible-at-k k %) data)) (range 10))]
+    (->> (apply u/map-grid #(nth %& %) data cache)
+         flatten
+         (apply max))))
 
 (defn solution [input]
   (let [data (parse input)]
