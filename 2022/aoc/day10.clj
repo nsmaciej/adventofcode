229493b5fc3a7aa245ce72d817b802c23c471db9
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-12-10 11:12:12 +0100

    Day 10, do not parse before simulating (2022)

--- 2022/aoc/day10.clj
+++ 2022/aoc/day10.clj
@@ -1,38 +1,28 @@
 (ns aoc.day10
-  (:require [clojure.core.match :refer [match]]
-            [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
-(defn- parse-line [line]
-  (let [[op arg] (str/split line #"\s+")]
-    (case op
-      "addx" [:addx (parse-long arg)]
-      "noop" [:noop])))
-
-(defn- parse [input]
-  (map parse-line (str/split-lines input)))
-
-(defn- simulate [states inst]
-  (let [x-value (last states)]
-    (match inst
-      [:addx arg] [x-value (+ x-value arg)]
-      [:noop]     [x-value])))
+(defn- simulate [x-value line]
+  (let [[inst op] (str/split line #" ")]
+    (case inst
+      "addx" [x-value (+ x-value (parse-long op))]
+      "noop" [x-value])))
 
 (defn- part-1 [x-values]
   (let [steps [20 60 100 140 180 220]]
     (apply + (map #(* % (nth x-values (dec %))) steps))))
 
 (defn- part-2 [x-values]
-  (->> (range (* 40 60))
-       (map (fn [x t] (if (>= 1 (abs (- x (mod t 40)))) "#" " "))
-            x-values)
+  (->> (map #(if (<= -1 (- %1 (mod %2 40)) 1) \# \space)
+            x-values
+            (range (* 40 60)))
        (partition 40)
-       (map #(apply str %))
+       (map str/join)
        (str/join "\n")))
 
 (defn solution [input]
   (let [x-values (->> input
-                      parse
-                      (reductions simulate [1])
-                      flatten
+                      str/split-lines
+                      (reductions #(simulate (last %1) %2) [1])
+                      (apply concat)
                       vec)]
-    [(part-1 x-values) (part-2 x-values)]))
+    [(part-1 x-values), (part-2 x-values)]))
