b3a019be1246c0194665e7fd60222654a38851c6
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-12-12 03:54:36 +0100

    Add quick-bench mode (2022)

--- 2022/aoc/core.clj
+++ 2022/aoc/core.clj
@@ -31,16 +31,23 @@
   [["-d" "--day DAY" "Day number"
     :parse-fn parse-long
     :validate [#(<= 1 % 24) "Must be a valid day 1-24"]]
-   ["-t" "--time" "Time the solutions"]])
+   ["-t" "--time" "Time the solutions"]
+   ["-q" "--quick" "Quick-bench the solutions"]])
 
-(defn- run-day [day & {:keys [time]}]
+(defn- run-day [day & {:keys [time quick]}]
+  (printf "=== Day %d ===\n" day)
   (let [input (u/input day)
-        start (System/nanoTime)
-        result ((day->solution day) input)
-        end (System/nanoTime)]
-    (run! println result)
-    (when time
-      (printf "Took %.2f ms\n" (/ (- end start) 1000000.0)))))
+        solution (day->solution day)]
+    ;; Use vec on the solution to make sure we are not timing
+    ;; how long it takes to build a lazy list.
+    (if quick
+      (criterium/quick-bench (vec (solution input)))
+      (let [start (System/nanoTime)
+            result (vec (solution input))
+            end (System/nanoTime)]
+        (run! println result)
+        (when time
+          (printf "Took %.2f ms\n" (/ (- end start) 1000000.0)))))))
 
 (defn -main [& args]
   (let [{:keys [options errors]} (parse-opts args cli-options)]
@@ -49,4 +56,5 @@
       (System/exit 1))
     (if-let [day (:day options)]
       (run-day day options)
-      (run! #(run-day % options) (range 1 (inc (count day->solution)))))))
+      (run! #(run-day % options) (range 1 (inc (count day->solution)))))
+    (shutdown-agents)))
