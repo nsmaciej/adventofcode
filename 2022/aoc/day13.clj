79e0cf26ee274cccce03b92194e4c32664eeadbe
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-12-16 02:40:36 +0100

    Day 13 part 2 + use EDN for parsing (2022)

--- 2022/aoc/day13.clj
+++ 2022/aoc/day13.clj
@@ -1,22 +1,36 @@
 (ns aoc.day13
   (:require [aoc.utils :as u]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.edn :as edn]))
 
 (defn- compare-packets [lhs rhs]
   (if (and (int? lhs) (int? rhs))
     (compare lhs rhs)
-    (let [lhs* (if (seqable? lhs) lhs [lhs])
-          rhs* (if (seqable? rhs) rhs [rhs])
+    (let [lhs* (if (int? lhs) [lhs] lhs)
+          rhs* (if (int? rhs) [rhs] rhs)
           cmps (map compare-packets lhs* rhs*)]
       (or (first (filter (complement zero?) cmps))
           (compare (count lhs*) (count rhs*))))))
 
-(defn- compare-pair [pair]
-  (->> (str/split-lines pair)
-       (map read-string)
-       (apply compare-packets)))
+(defn part-1 [packets]
+  (->> packets
+       (partition 2)
+       (map #(apply compare-packets %))
+       (keep-indexed #(when (neg? %2) (inc %1)))
+       (apply +)))
 
-(->> (str/split (u/input 13) #"\n\n")
-     (map compare-pair)
-     (keep-indexed #(when (<= %2 0) (inc %1)))
-     (apply +))
+(defn part-2 [packets]
+  (let [dividers #{[[2]] [[6]]}]
+    (->> packets
+         (concat dividers)
+         (sort compare-packets)
+         (keep-indexed #(when (dividers %2) (inc %1)))
+         (apply *))))
+
+(defn- solution [input]
+  (let [packets (->> (str/split-lines input)
+                     (filter seq)
+                     (map edn/read-string))]
+    [(part-1 packets) (part-2 packets)]))
+
+(u/register 13 solution)
