1797ae1eca432a4240718bf6b40621cca0d08ff1
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-12-21 23:03:38 +0100

    Day 20, using Java™ powers, runs in 1 second now (2022)

--- 2022/aoc/day20.clj
+++ 2022/aoc/day20.clj
@@ -1,33 +1,24 @@
 (ns aoc.day20
-  (:require [clojure.core.rrb-vector :as fv]
-            [aoc.utils :as u]))
+  (:require [aoc.utils :as u])
+  (:import java.util.LinkedList))
 
-(defn- fv-pop-at [coll i]
-  (fv/catvec (fv/subvec coll 0 i)
-             (fv/subvec coll (inc i))))
-
-(defn- fv-insert-at [coll i value]
-  (fv/catvec (fv/subvec coll 0 i)
-             (fv/vector value)
-             (fv/subvec coll i)))
-
-(defn- fv-index-of [coll needle]
-  (first (keep-indexed #(when (= %2 needle) %1) coll)))
-
-(defn- mix-indices [data ixs ix]
+(defn- mix-indices! [data ^LinkedList ixs ix]
   (let [shift (nth data ix)
-        from (fv-index-of ixs ix)
+        from (.indexOf ixs ix)
         to (mod (+ ix shift) (dec (count data)))]
-    (fv-insert-at (fv-pop-at ixs from) to ix)))
+    (doto ixs
+      (.remove (int from))
+      (.add to ix))))
 
 (defn- mix [data times]
   (let [n (count data)
-        mixed (->> (map #(mod % n) (range (* n times)))
-                   (reduce #(mix-indices data %1 %2) (fv/vec (range n)))
-                   (mapv #(nth data %)))
-        zero-ix (fv-index-of mixed 0)]
-    (apply + (map #(nth mixed (mod (+ zero-ix %) n))
-                  [1000 2000 3000]))))
+        ixs (LinkedList. (range n))]
+    (dotimes [ix (* n times)]
+      (mix-indices! data ixs (mod ix n)))
+    (let [mixed (mapv #(nth data %) ixs)
+          zero-ix (.indexOf mixed 0)]
+      (apply + (map #(nth mixed (mod (+ zero-ix %) n))
+                    [1000 2000 3000])))))
 
 (defn- solution [input]
   (let [data (mapv parse-long (re-seq #"-?\d+" input))]
