f12799d98ceab6e4f8c3a2b0b8faa1ceef1ad3e3
Maciej Goszczycki <mgoszcz2@gmail.com>
2022-12-23 16:15:22 +0100

    Day 21,  Get rid of names after solve and use :humn as the sentinel value (2022)

--- 2022/aoc/day21.clj
+++ 2022/aoc/day21.clj
@@ -6,50 +6,45 @@
 (def ^:private parse-op {"+" :plus "-" :minus "*" :times "/" :divide})
 
 (defn- parse-riddle [input]
-  (into
-   {}
-   (for [line (str/split-lines input)]
-     (let [[name x op rhs :as words] (str/split line #":? +")]
-       [(keyword name)
-        (if (= (count words) 2)
-          {:n (parse-long x)}
-          {:lhs (keyword x), :op (parse-op op), :rhs (keyword rhs)})]))))
+  (for [line (str/split-lines input)]
+    (let [[name x op rhs :as words] (str/split line #":? +")]
+      [(keyword name)
+       (if (= (count words) 2)
+         {:n (parse-long x)}
+         {:lhs (keyword x), :op (parse-op op), :rhs (keyword rhs)})])))
 
 (defn- solve [riddle name]
   (let [m (riddle name)]
-    (if (contains? m :n)
-      (assoc m :name name, :value (:n m))
-      (let [{x :value :as lhs} (solve riddle (:lhs m))
-            {y :value :as rhs} (solve riddle (:rhs m))]
-        (assoc m
-               :name name :lhs lhs :rhs rhs
-               :value (if (and x y)
-                        (case (:op m)
-                          :plus (+ x y)
-                          :minus (- x y)
-                          :times (* x y)
-                          :divide (/ x y))
-                        nil))))))
+    (if (:n m)
+      (assoc m :val (:n m))
+      (let [{x :val :as lhs} (solve riddle (:lhs m))
+            {y :val :as rhs} (solve riddle (:rhs m))]
+        (assoc m :lhs lhs :rhs rhs :val (if (some #{:humn} [x y])
+                                          :humn
+                                          (case (:op m)
+                                            :plus (+ x y)
+                                            :minus (- x y)
+                                            :times (* x y)
+                                            :divide (/ x y))))))))
 
 (defn- unify [m x]
-  (if (= (:name m) :humn)
+  (if (= (:n m) :humn)
     x ; We solved it.
     (let [{:keys [lhs op rhs]} m]
-      (match [(:value lhs) op (:value rhs)]
-        [nil :plus   y] (recur lhs (- x y))
-        [nil :minus  y] (recur lhs (+ x y))
-        [nil :times  y] (recur lhs (/ x y))
-        [nil :divide y] (recur lhs (* x y))
-        [y :plus   nil] (recur rhs (- x y))
-        [y :minus  nil] (recur rhs (- y x))
-        [y :times  nil] (recur rhs (/ x y))
-        [y :divide nil] (recur rhs (/ y x))))))
+      (match [(:val lhs) op (:val rhs)]
+        [:humn :plus   y] (recur lhs (- x y))
+        [:humn :minus  y] (recur lhs (+ x y))
+        [:humn :times  y] (recur lhs (/ x y))
+        [:humn :divide y] (recur lhs (* x y))
+        [y :plus   :humn] (recur rhs (- x y))
+        [y :minus  :humn] (recur rhs (- y x))
+        [y :times  :humn] (recur rhs (/ x y))
+        [y :divide :humn] (recur rhs (/ y x))))))
 
 (defn- solution [input]
-  (let [riddle (parse-riddle input)]
-    [(:value (solve riddle :root))
-     (let [s (solve (assoc-in riddle [:humn :n] nil) :root)]
-       (unify (:lhs s) (-> s :rhs :value)))]))
+  (let [riddle (into {} (parse-riddle input))]
+    [(:val (solve riddle :root))
+     (let [s (solve (assoc-in riddle [:humn :n] :humn) :root)]
+       (unify (:lhs s) (-> s :rhs :val)))]))
 
 (u/add-solution 21 solution)
-(u/solve 21)
