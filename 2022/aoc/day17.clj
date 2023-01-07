778ff9b841b6d366a3e602dde91368d7348fe25d
Maciej Goszczycki <mgoszcz2@gmail.com>
2023-01-07 05:52:57 +0100

    Day 17, 2x speedup using defrecord (2022)

--- 2022/aoc/day17.clj
+++ 2022/aoc/day17.clj
@@ -2,25 +2,28 @@
   (:require [aoc.utils :as u]
             [clojure.string :as str]))
 
-(def ^:private start-offset [3 2])
+(defrecord Point [y x])
+(defn- +p [p1 p2] (->Point (+ (:y p1) (:y p2)) (+ (:x p1) (:x p2))))
+
+(def ^:private start-offset (->Point 3 2))
 (def ^:private field-width 7)
 
 ;; Written bottom-to-top right-to-left. We think ltr but pack bits rtl.
-(def shapes
+(def ^:private shapes
   [{:rows [2r1111] :width 4}
    {:rows [2r010 2r111 2r010] :width 3}
    {:rows [2r111 2r100 2r100] :width 3}
    {:rows [2r1 2r1 2r1 2r1] :width 1}
    {:rows [2r11 2r11] :width 2}])
 
-(defn- collides? [field shape [y x]]
+(defn- collides? [field shape {:keys [y x]}]
   (or (neg? y) (neg? x) (> (+ x (:width shape)) field-width) ; Walls.
       (and (< y (count field)) ; Fast path: the shape is above the y-start line.
            (->> (:rows shape)
                 (map-indexed #(bit-and (bit-shift-left %2 x) (nth field (+ y %1) 0)))
                 (some pos?)))))
 
-(defn- glue-shape [field rows [y x]]
+(defn- glue-shape [field rows {:keys [y x]}]
   (let [grow-by (- (+ y (count rows)) (count field))
         field (if (pos? grow-by) (into field (repeat grow-by 0)) field)]
     (reduce #(update %1 (+ y %2) bit-or (bit-shift-left (rows %2) x))
@@ -31,12 +34,11 @@
   (let [shape-ix (mod shape-ix (count shapes))
         shape (nth shapes shape-ix)]
     (loop [nudge-ix nudge-ix
-           pos-start (u/+p [(count field) 0] start-offset)]
+           pos-start (+p (->Point (count field) 0) start-offset)]
       (let [nudge-ix (mod nudge-ix (count nudges))
-            nudge (nth nudges nudge-ix)
-            pos-nudged (u/+p pos-start nudge)
+            pos-nudged (+p pos-start (nth nudges nudge-ix))
             pos (if (collides? field shape pos-nudged) pos-start pos-nudged)
-            pos-fallen (u/-p pos [1 0])]
+            pos-fallen (+p pos (->Point -1 0))]
         (if (collides? field shape pos-fallen)
           {:field (glue-shape field (:rows shape) pos)
            :nudge-ix (inc nudge-ix)
@@ -50,7 +52,7 @@
      (:shape step)
      (reduce bit-or 0 (map-indexed #(bit-shift-left %2 (* %1 field-width)) rows))]))
 
-(defn find-cycle [seen state]
+(defn- find-cycle [seen state]
   (let [key (step-key state), step (assoc state :step (count seen))]
     (if-let [matching-step (get seen key)]
       (reduced [matching-step step])
@@ -72,7 +74,7 @@
          (+ (* cycle-growth cycles)))))
 
 (defn- solution [input]
-  (let [nudges (mapv {\> [0 1] \< [0 -1]} (str/trim input))
+  (let [nudges (mapv {\> (->Point 0 1), \< (->Point 0 -1)} (str/trim input))
         start {:field (vector-of :long), :nudge-ix 0}
         states (reductions #(drop-shape nudges %1 %2) start (range))]
     [(-> states (nth 2022) :field count), (part-2 nudges states)]))
