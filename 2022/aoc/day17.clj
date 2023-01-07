6be0d1d48228c9557b888b2f0a287c29ad2687d6
Maciej Goszczycki <mgoszcz2@gmail.com>
2023-01-07 07:55:17 +0100

    Day 17, get rid of `field-key` and notice we need no simulation for part 2 (2022)

--- 2022/aoc/day17.clj
+++ 2022/aoc/day17.clj
@@ -7,6 +7,7 @@
 
 (def ^:private start-offset (->Point 3 2))
 (def ^:private field-width 7)
+(def ^:private cycle-check-rows 6)
 
 ;; Written bottom-to-top right-to-left. We think ltr but pack bits rtl.
 (def ^:private shapes
@@ -20,12 +21,14 @@
   (or (neg? y) (neg? x) (> (+ x (:width shape)) field-width) ; Walls.
       (and (< y (count field)) ; Fast path: the shape is above the y-start line.
            (->> (:rows shape)
-                (map-indexed #(bit-and (bit-shift-left %2 x) (nth field (+ y %1) 0)))
+                (map-indexed #(bit-and (bit-shift-left %2 x)
+                                       (nth field (+ y %1) 0)))
                 (some pos?)))))
 
 (defn- glue-shape [field rows {:keys [y x]}]
   ;; Update one past the end gives us a nil.
-  (reduce (fn [f r] (update f (+ y r) #(bit-or (or % 0) (bit-shift-left (rows r) x))))
+  (reduce (fn [f r]
+            (update f (+ y r) #(bit-or (or % 0) (bit-shift-left (rows r) x))))
           field
           (range (count rows))))
 
@@ -45,16 +48,14 @@
            :step (inc step)}
           (recur (inc nudge-ix) pos-fallen))))))
 
-(defn- field-key [field]
-  (let [rows (subvec field (max 0 (- (count field) (quot 64 field-width))))]
-    (transduce (map-indexed #(bit-shift-left %2 (* %1 field-width)))
-               (completing bit-or) 0 rows)))
-
 (defn- find-cycle [seen state]
-  (let [key [(:nudge-ix state) (:shape-ix state) (field-key (:field state))]]
-    (if-let [state* (get seen key)] (reduced [state* state]) (assoc seen key state))))
+  (let [ix (max 0 (- (count (:field state)) cycle-check-rows))
+        key [(:nudge-ix state) (:shape-ix state) (subvec (:field state) ix)]]
+    (if-let [state* (get seen key)]
+      (reduced [state* state])
+      (assoc seen key state))))
 
-(defn- part-2 [nudges states]
+(defn- part-2 [states]
   (let [part-2-steps 1000000000000
         [start end] (reduce find-cycle {} states)
         start-step (:step start)
@@ -62,17 +63,13 @@
         cycle-steps (- (:step end) start-step)
         cycles (quot (- part-2-steps start-step) cycle-steps)
         steps-left (- part-2-steps (* cycles cycle-steps) start-step)]
-    ;; Simulate the final steps-left steps and add the cycles we "cut".
-    (->> (range steps-left)
-         (reduce #(drop-shape nudges %1 (+ (:shape-ix start) %2)) start)
-         :field
-         count
-         (+ (* cycle-growth cycles)))))
+    (+ (-> states (nth (+ start-step steps-left)) :field count)
+       (* cycle-growth cycles))))
 
 (defn- solution [input]
   (let [nudges (mapv {\> (->Point 0 1), \< (->Point 0 -1)} (str/trim input))
         start {:field (vector-of :long), :nudge-ix 0, :step 0}
         states (reductions #(drop-shape nudges %1 %2) start (range))]
-    [(-> states (nth 2022) :field count), (part-2 nudges states)]))
+    [(-> states (nth 2022) :field count), (part-2 states)]))
 
 (u/add-solution 17 solution)
